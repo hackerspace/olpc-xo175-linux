b4ac7b54ca099a503fcced75b356e61b25da8082
Chao Xie <chao.xie@marvell.com>
2011-06-23 15:13:38 +0800

    ARM: PXA95X: add common regs definition for pxa95x
    Change-Id: I47476c2a72821f524c12259c266a68a0a2b331a4
    Signed-off-by: Chao Xie <chao.xie@marvell.com>

--- arch/arm/mach-pxa/include/mach/pxa3xx-regs.h
+++ arch/arm/mach-pxa/include/mach/pxa3xx-regs.h
@@ -20,8 +20,20 @@
  */
 #define OSCC           __REG(0x41350000)  /* Oscillator Configuration Register */
 
-#define OSCC_PEN       (1 << 11)       /* 13MHz POUT */
+#define OSCC_VCTVSTB_OFFSET (20)
+#define OSCC_VCTVCEN	(1 << 23)
+#define OSCC_VCTVSTB	(1 << OSCC_VCTVSTB_OFFSET)
+#define OSCC_PEN	(1 << 11)		/* 13MHz POUT */
+#define OSCC_TENS3	(1 << 10)
+#define OSCC_TENS2	(1 << 9)
+#define OSCC_TENS0	(1 << 8)
 
+/*
+ * Main Clock Control Unit Registers
+ */
+#define DMEMVLR		__REG(0x4135000C)	/* DMC Voltage Level Configuration Register */
+
+#define DMEMVLR_DMCHV_OFFSET (0)
 
 /*
  * Service Power Management Unit (MPMU)
@@ -33,9 +45,33 @@
 #define PWER		__REG(0x40F50010)	/* Power Manager Wake-up Enable Register */
 #define PWSR		__REG(0x40F50014)	/* Power Manager Wake-up Status Register */
 #define PECR		__REG(0x40F50018)	/* Power Manager EXT_WAKEUP[1:0] Control Register */
+#define OVH		__REG(0x40F50020)	/* Overheating Control Register */
 #define DCDCSR		__REG(0x40F50080)	/* DC-DC Controller Status Register */
+#define SDCR		__REG(0x40F5008C)	/* SRAM State-retentive Control Register */
+#define AVCR		__REG(0x40F50094)	/* VCC_MAIN Voltage Control Register */
+#define SVCR		__REG(0x40F50098)	/* VCC_SRAM Voltage Control Register */
+#define PSBR		__REG(0x40F500A0)	/* Power Manager Safty Bits Register */
 #define PVCR		__REG(0x40F50100)	/* Power Manager Voltage Change Control Register */
 #define PCMD(x)		__REG(0x40F50110 + ((x) << 2))
+
+#define PMCR_BIE	(1 << 0)		/* Interrupt Enable for nBATT_FAULT */
+#define PMCR_BIS	(1 << 1)		/* Interrupt Status for nBATT_FAULT */
+#define PMCR_TIE	(1 << 10)		/* Interrupt Enable for XScale Core Frequency Change */
+#define PMCR_TIS	(1 << 11)		/* Interrupt Status for XScale Core Frequency Change */
+#define PMCR_VIE	(1 << 12)		/* Interrupt Enable for VCC_APPS and VCC_SRAM Voltage Change */
+#define PMCR_VIS	(1 << 13)		/* Interrupt Status for VCC_APPS and VCC_SRAM Voltage Change */
+#define PMCR_SWGR	(1 << 31)		/* Software GPIO Reset */
+#define PSR_TSS_OFF	(12)
+#define OVH_TEMP_EN	(1 << 0)		/* Enable for Temperature Sensor */
+#define OVH_OWM		(1 << 7)		/* Over-heating WDT Enable */
+#define OVH_OVWF_OFF	(4)			/* WDT Reset Temperature Over-heating Threshold */
+#define OVH_OTIF_OFF	(1)			/* Over-heating Treshold Value for Generating TIS Software Interrupt */
+#define PVCR_VCSA	(1 << 14)
+
+#define AVCR_ALVL3_OFFSET 24
+#define AVCR_ALVL3_MASK (0x1F << AVCR_ALVL3_OFFSET)
+#define CORE_OVERHEATING_DETECTED 1
+#define CORE_COLLING_DETECTED 0
 
 /*
  * Slave Power Management Unit
@@ -54,6 +90,20 @@
 #define AD3R		__REG(0x40f40030)	/* Application Subsystem D3 Configuration */
 #define AD2R		__REG(0x40f40034)	/* Application Subsystem D2 Configuration */
 #define AD1R		__REG(0x40f40038)	/* Application Subsystem D1 Configuration */
+#define ACGD0ER		__REG(0x40F40040)	/* Application Subsystem CG to D0 state Wakeup Status Register */
+#define ACGD0SR		__REG(0x40F40044)	/* Application Subsystem CG to D0 state Wakeup Status Register */
+#define PWRMODE		__REG(0x40F40080)	/* Application Subsystem Power Mode Register */
+#define CPUPWR		__REG(0x40F40084)	/* Application Subsystem CPU Power Mode Register */
+#define VMPWR		__REG(0x40F40090)	/* Application Subsystem VMeta Power Mode Register */
+#define GCPWR		__REG(0x40F40094)	/* Application Subsystem GCU Power Mode Register */
+
+#define VMPWR_PWON		(1 << 0)
+#define VMPWR_PWR_ST		(1 << 2)
+#define VMPWR_SETALLWAYS	(0xFF00)
+#define GCPWR_PWON		(1 << 0)
+#define GCPWR_RST_N		(1 << 1)
+#define GCPWR_PWR_ST		(1 << 2)
+#define GCPWR_SETALLWAYS	(0xFF00)
 
 /*
  * Application Subsystem Configuration bits.
@@ -119,6 +169,7 @@
  */
 #define PXA3xx_PM_S3D4C4	0x07	/* aka deep sleep */
 #define PXA3xx_PM_S2D3C4	0x06	/* aka sleep */
+#define PXA3xx_PM_S0D0CS	0x05	/* aka D0CS */
 #define PXA3xx_PM_S0D2C2	0x03	/* aka standby */
 #define PXA3xx_PM_S0D1C2	0x02	/* aka LCD refresh */
 #define PXA3xx_PM_S0D0C1	0x01
@@ -132,28 +183,56 @@
 #define CKENA		__REG(0x4134000C)	/* A Clock Enable Register */
 #define CKENB		__REG(0x41340010)	/* B Clock Enable Register */
 #define AC97_DIV	__REG(0x41340014)	/* AC97 clock divisor value register */
+#define ACCR1		__REG(0x41340020)	/* Application Subsystem Clock Configuration Register 1 */
+#define CKENC		__REG(0x41340024)	/* C Clock Enable Register */
+#define CCLKCFG		__REG(0x41340040)	/* Core Clock Configuration Register */
 
 #define ACCR_XPDIS		(1 << 31)	/* Core PLL Output Disable */
 #define ACCR_SPDIS		(1 << 30)	/* System PLL Output Disable */
 #define ACCR_D0CS		(1 << 26)	/* D0 Mode Clock Select */
 #define ACCR_PCCE		(1 << 11)	/* Power Mode Change Clock Enable */
 #define ACCR_DDR_D0CS		(1 << 7)	/* DDR SDRAM clock frequency in D0CS (PXA31x only) */
+#define ACCR_DMCFS_312		(1 << 6)	/* DDR SDRAM clock frequency 312MHz */
 
+#define ACCR_XPDIS_MASK		(0x1 << 31)	/* Core PLL Output Disable */
+#define ACCR_SPDIS_MASK		(0x1 << 30)	/* System PLL Output Disable */
+#define ACCR_AXIFS_MASK		(0x3 << 28)	/* AXI Bus Frequency Select */
+#define ACCR_D0CS_MASK		(0x1 << 26)	/* D0 Mode Clock Select */
 #define ACCR_SMCFS_MASK		(0x7 << 23)	/* Static Memory Controller Frequency Select */
+#define ACCR_GCFS_MASK		(0x3 << 20)	/* Graphics Controller Frequency Select */
 #define ACCR_SFLFS_MASK		(0x3 << 18)	/* Frequency Select for Internal Memory Controller */
 #define ACCR_XSPCLK_MASK	(0x3 << 16)	/* Core Frequency during Frequency Change */
 #define ACCR_HSS_MASK		(0x3 << 14)	/* System Bus-Clock Frequency Select */
 #define ACCR_DMCFS_MASK		(0x3 << 12)	/* Dynamic Memory Controller Clock Frequency Select */
 #define ACCR_XN_MASK		(0x7 << 8)	/* Core PLL Turbo-Mode-to-Run-Mode Ratio */
+#define ACCR_DDR_D0CS_MASK	(0x1 << 7)	/* Memory Clock in D0CS */
+#define ACCR_DMCFS_312_MASK	(0x1 << 6)	/* DMC PLL Select */
+#ifdef CONFIG_CPU_PXA3XX
 #define ACCR_XL_MASK		(0x1f)		/* Core PLL Run-Mode-to-Oscillator Ratio */
+#else
+#define ACCR_XL_MASK		(0x3f)		/* Core PLL Run-Mode-to-Oscillator Ratio */
+#endif
 
+#define ACCR_AXI(x)		(((x) & 0x3) << 28)
 #define ACCR_SMCFS(x)		(((x) & 0x7) << 23)
+#define ACCR_GCFS(x)		(((x) & 0x3) << 20)
 #define ACCR_SFLFS(x)		(((x) & 0x3) << 18)
 #define ACCR_XSPCLK(x)		(((x) & 0x3) << 16)
 #define ACCR_HSS(x)		(((x) & 0x3) << 14)
 #define ACCR_DMCFS(x)		(((x) & 0x3) << 12)
 #define ACCR_XN(x)		(((x) & 0x7) << 8)
-#define ACCR_XL(x)		((x) & 0x1f)
+#define ACCR_XL(x)		((x) & ACCR_XL_MASK)
+
+#define ACCR1_DIS_DRX		(1 << 31)	/* Disable DRX */
+#define ACCR1_VMETA_156_312	(1 << 21)	/* VMeta Frequency Control: 0 = 156Mhz, 1 = 312Mhz */
+#define ACCR1_PU_OTG		(1 << 12)	/* USB 2.0 PHY OTG power up */
+#define ACCR1_PU_PLL		(1 << 11)	/* USB 2.0 PHY PLL power up */
+#define ACCR1_PU		(1 << 10)	/* USB 2.0 PHY power up */
+#define ACCR1_I2C_33_52		(1 << 8)	/* I2C frequency control: 0 = 624/19 Mhz, 1 = 624/12 Mhz */
+#define ACCR1_MMC6_48_52	(1 << 6)	/* MMC6 frequency control: 0 = 624/13 Mhz, 1 = 624/12 Mhz */
+#define ACCR1_MMC5_48_52	(1 << 4)	/* MMC5 frequency control: 0 = 624/13 Mhz, 1 = 624/12 Mhz */
+#define ACCR1_MMC4_48_52	(1 << 2)	/* MMC4 frequency control: 0 = 624/13 Mhz, 1 = 624/12 Mhz */
+#define ACCR1_MMC3_48_52	(1 << 0)	/* MMC3 frequency control: 0 = 624/13 Mhz, 1 = 624/12 Mhz */
 
 /*
  * Clock Enable Bit
@@ -187,6 +266,8 @@
 #define CKEN_MSL0	30	/* < MSL0 clock enable */
 #define CKEN_PWM0	32	/* < PWM[0] clock enable */
 #define CKEN_PWM1	33	/* < PWM[1] clock enable */
+#define CKEN_HSI	34	/* < HSI clock enable */
+#define CKEN_VMETA	35	/* < VMeta clock enable */
 #define CKEN_I2C	36	/* < I2C clock enable */
 #define CKEN_INTC	38	/* < Interrupt controller clock enable */
 #define CKEN_GPIO	39	/* < GPIO clock enable */
@@ -194,6 +275,34 @@
 #define CKEN_HSIO2	41	/* < HSIO2 clock enable */
 #define CKEN_MINI_IM	48	/* < Mini-IM */
 #define CKEN_MINI_LCD	49	/* < Mini LCD */
+#define CKEN_ABU	59	/* < ABU clock enable */
+#define CKEN_HSIO	61	/* < System Bus (HSIO) clock enable */
+#define CKEN_CSI_TX	64	/* < CSI TX Escape clock enable */
+#define CKEN_PXA95x_MMC1	65	/* < MMC1 clock enable */
+#define CKEN_PXA95x_MMC2	66	/* < MMC2 clock enable */
+#define CKEN_PXA95x_MMC3	67	/* < MMC3 clock enable */
+#define CKEN_PXA95x_MMC4	68	/* < MMC4 clock enable */
+#define CKEN_AXI_2X	69	/* < AXI 2X clock enable */
+#define CKEN_USB_PRL	70	/* < USB2(OTG) Peripheral clock enable */
+#define CKEN_USBH_PRL	71	/* < USB2 Host1 Peripheral clock enable */
+#define CKEN_USB_BUS	74	/* < USB2(OTG) System Bus clock enable */
+#define CKEN_USBH_BUS	75	/* < USB2 Host1 System Bus clock enable */
+#define CKEN_MMC1_BUS	78	/* < MMC1 System Bus clock enable */
+#define CKEN_MMC2_BUS	79	/* < MMC2 System Bus clock enable */
+#define CKEN_MMC3_BUS	80	/* < MMC3 System Bus clock enable */
+#define CKEN_MMC4_BUS	81	/* < MMC4 System Bus clock enable */
+#define CKEN_IMU	82	/* < ISLAND MMC USB System Bus clock enable */
+#define CKEN_AXI	83	/* < AXI clock enable */
+#define CKEN_DISPLAY	86	/* < Display clock enable */
+#define CKEN_PIXEL	87	/* < Pixel clock enable */
+#define CKEN_I2C2	88	/* < I2C2 clock enable */
+#define CKEN_I2C3	89	/* < I2C3 clock enable */
+#define CKEN_SCI1	90	/* < Camera SCI1 clock enable */
+#define CKEN_SCI2	91	/* < Camera SCI2 clock enable */
+#define CKEN_GC_1X	92	/* < Graphics 1x clock enable */
+#define CKEN_GC_2X	93	/* < Graphics 2x clock enable */
+#define CKEN_DSI_TX1	94	/* < DSI TX1 Escape clock enable */
+#define CKEN_DSI_TX2	95	/* < DSI TX2 Escape clock enable */
 
 #define CKEN_MMC3	5	/* < MMC3 Clock Enable */
 #define CKEN_MVED	43	/* < MVED clock enable */
@@ -201,5 +310,11 @@
 /* Note: GCU clock enable bit differs on PXA300/PXA310 and PXA320 */
 #define CKEN_PXA300_GCU		42	/* Graphics controller clock enable */
 #define CKEN_PXA320_GCU		7	/* Graphics controller clock enable */
+
+#define CKEN_PWM4	0	/* < PWMCCR4 offset  */
+#define CKEN_PWM5	4	/* < PWMCCR4 offset  */
+#define CKEN_PWM6	8	/* < PWMCCR4 offset  */
+#define CKEN_PWM7	12	/* < PWMCCR4 offset  */
+#define PWMCLKEN_SLOW	(1 << 6)	/* PWM SLOW CLK enble Reset */
 
 #endif /* __ASM_ARCH_PXA3XX_REGS_H */
