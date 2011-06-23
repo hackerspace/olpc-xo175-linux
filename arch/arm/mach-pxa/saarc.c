fd8f811adc074eef09253c0736a4d83bddfb4d9e
Chao Xie <chao.xie@marvell.com>
2011-06-23 11:58:47 +0800

    ARM: pxa970: saarc: add saarc board support
    Change-Id: If8873ef9d71bf695ffbd2dfbcd5f36bf926d5e03
    Signed-off-by: Chao Xie <chao.xie@marvell.com>

---
+++ arch/arm/mach-pxa/saarc.c
@@ -0,0 +1,223 @@
+/*
+ *  linux/arch/arm/mach-pxa/saarc.c
+ *
+ *  Support for the Marvell Handheld Platform (aka SAAR C2)
+ *
+ *  Copyright (C) 2007-2010 Marvell International Ltd.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  publishhed by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/i2c.h>
+#include <linux/i2c/pxa95x-i2c.h>
+#include <linux/mfd/88pm860x.h>
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/mmc/sdhci.h>
+
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+
+#include <mach/hardware.h>
+#include <mach/mfp.h>
+#include <mach/gpio.h>
+#include <mach/mfp-pxa930.h>
+#include <mach/pxa95xfb.h>
+
+#include <plat/pxa27x_keypad.h>
+
+#include "devices.h"
+#include "generic.h"
+
+#define NEVOSAARC_NR_IRQS	(IRQ_BOARD_START + 40)
+
+#if defined(CONFIG_TOUCHSCREEN_VNC)
+static struct platform_device vnc_device = {
+	.name	= "vnc-ts",
+	.id	= -1,
+};
+#endif
+
+static struct pm860x_backlight_pdata backlight[] = {
+	{
+		/*backlight data*/
+		.id	= PM8606_ID_BACKLIGHT,
+		.iset = PM8606_WLED_CURRENT(0x12),
+		.flags	= PM8606_BACKLIGHT1,
+	},
+	{
+		/*keypad backlight data*/
+		.id	= PM8606_ID_BACKLIGHT,
+		.pwm = 0x10,
+		.iset = PM8606_WLED_CURRENT(0x05),
+		.flags	= PM8606_BACKLIGHT2,
+	},
+};
+
+static struct pm860x_led_pdata led[] = {
+	{
+		.id	= PM8606_ID_LED,
+		.iset	= PM8606_LED_CURRENT(12),
+		.flags	= PM8606_LED1_RED,
+	}, {
+		.id	= PM8606_ID_LED,
+		.iset	= PM8606_LED_CURRENT(12),
+		.flags	= PM8606_LED1_GREEN,
+	}, {
+		.id	= PM8606_ID_LED,
+		.iset	= PM8606_LED_CURRENT(12),
+		.flags	= PM8606_LED1_BLUE,
+	}, {
+		.id	= PM8606_ID_LED,
+		.iset	= PM8606_LED_CURRENT(12),
+		.flags	= PM8606_LED2_RED,
+	}, {
+		.id	= PM8606_ID_LED,
+		.iset	= PM8606_LED_CURRENT(12),
+		.flags	= PM8606_LED2_GREEN,
+	}, {
+		.id	= PM8606_ID_LED,
+		.iset	= PM8606_LED_CURRENT(12),
+		.flags	= PM8606_LED2_BLUE,
+	},
+};
+
+#if defined(CONFIG_SENSORS_CM3601)
+static int cm3601_request_resource(unsigned char gpio_num, char *name)
+{
+	int ret = 0;
+	ret = gpio_request(mfp_to_gpio(gpio_num), name);
+	if (ret) {
+			printk(KERN_ERR "%s: can't request GPIO %d.\n", __func__, gpio_num);
+			return -1;
+	}
+	return ret;
+}
+
+static void cm3601_release_resource(unsigned char gpio_num)
+{
+       gpio_free(gpio_num);
+}
+
+static struct pm860x_cm3601_pdata cm3601_platform_info = {
+       .gpio_en = MFP_PIN_GPIO51,
+       .gpio_out = MFP_PIN_GPIO50,
+       .request_source = cm3601_request_resource,
+       .release_source = cm3601_release_resource,
+};
+#endif
+
+static struct pm860x_platform_data pm8607_info = {
+	.backlight	= &backlight[0],
+	.led		= &led[0],
+#if defined(CONFIG_SENSORS_CM3601)
+	.cm3601		= &cm3601_platform_info,
+#endif
+	.irq_mode	= 0,
+	.irq_base	= IRQ_BOARD_START,
+
+	.i2c_port	= PI2C_PORT,
+
+	.num_leds	= ARRAY_SIZE(led),
+	.num_backlights	= ARRAY_SIZE(backlight),
+};
+
+static struct i2c_board_info i2c1_info[] = {
+	{
+		.type		= "88PM860x",
+		.addr		= 0x34,
+		.platform_data	= &pm8607_info,
+		.irq		= gpio_to_irq(mfp_to_gpio(MFP_PIN_GPIO83)),
+	},
+};
+
+static struct i2c_pxa_platform_data i2c1_pdata = {
+	.use_pio        = 0,
+	.flags		= PXA_I2C_HIGH_MODE | PXA_I2C_FAST_MODE | PXA_I2C_USING_FIFO_PIO_MODE,
+	.master_code	= (0x08 | 0x06), /*8 -highest, 0xF -lowest arbitration*/
+};
+
+static struct i2c_pxa_platform_data i2c2_pdata = {
+	.use_pio	= 0,
+	.flags		= PXA_I2C_FAST_MODE | PXA_I2C_USING_FIFO_PIO_MODE,
+};
+
+static struct i2c_pxa_platform_data i2c3_pdata = {
+	.use_pio = 0,
+	.flags = PXA_I2C_FAST_MODE | PXA_I2C_USING_FIFO_PIO_MODE,
+};
+
+static struct platform_device *devices[] __initdata = {
+	&pxa95x_device_i2c1,
+	&pxa95x_device_i2c2,
+	&pxa95x_device_i2c3,
+#if defined(CONFIG_TOUCHSCREEN_VNC)
+	&vnc_device,
+#endif
+};
+
+#if defined(CONFIG_KEYBOARD_PXA27x) || defined(CONFIG_KEYBOARD_PXA27x_MODULE)
+static unsigned int matrix_key_map[] = {
+	/* KEY(row, col, key_code) */
+	KEY(0, 0, KEY_HOME),
+	KEY(0, 1, KEY_SEND),
+	KEY(0, 2, KEY_CAMERA),
+	KEY(1, 0, KEY_WWW),
+	KEY(1, 1, KEY_BACK),
+	KEY(1, 2, KEY_CAMERA),
+	KEY(2, 0, KEY_END),
+	KEY(2, 1, KEY_VOLUMEUP),
+	KEY(2, 2, KEY_VOLUMEUP),/*not exist*/
+	KEY(3, 0, KEY_MENU),
+	KEY(3, 1, KEY_VOLUMEDOWN),
+	KEY(3, 2, KEY_VOLUMEDOWN),/*not exists*/
+};
+
+static struct pxa27x_keypad_platform_data keypad_info = {
+	.matrix_key_rows = 4,
+	.matrix_key_cols = 3,
+	.matrix_key_map = matrix_key_map,
+	.matrix_key_map_size = ARRAY_SIZE(matrix_key_map),
+	.debounce_interval = 30,
+	.active_low = 1,
+	.direct_key_num = 5,
+	.direct_key_map = { KEY_RESERVED,
+						KEY_RESERVED,
+						KEY_RESERVED,
+						KEY_RESERVED,
+						KEY_ENTER },
+};
+#endif /* CONFIG_KEYBOARD_PXA27x || CONFIG_KEYBOARD_PXA27x_MODULE */
+
+static void __init init(void)
+{
+	pxa_set_ffuart_info(NULL);
+	platform_device_add_data(&pxa95x_device_i2c1, &i2c1_pdata,
+				 sizeof(i2c1_pdata));
+	platform_device_add_data(&pxa95x_device_i2c2, &i2c2_pdata,
+				 sizeof(i2c2_pdata));
+	platform_device_add_data(&pxa95x_device_i2c3, &i2c3_pdata,
+				 sizeof(i2c3_pdata));
+
+	platform_add_devices(ARRAY_AND_SIZE(devices));
+	i2c_register_board_info(0, ARRAY_AND_SIZE(i2c1_info));
+
+#if defined(CONFIG_KEYBOARD_PXA27x) || defined(CONFIG_KEYBOARD_PXA27x_MODULE)
+	pxa_set_keypad_info(&keypad_info);
+#endif
+}
+
+MACHINE_START(NEVOSAARC, "PXA970 Handheld Platform (aka SAAR C2)")
+	.map_io		= pxa_map_io,
+	.nr_irqs	= NEVOSAARC_NR_IRQS,
+	.init_irq	= pxa95x_init_irq,
+	.handle_irq	= pxa95x_handle_irq_intc,
+	.timer		= &pxa_timer,
+	.init_machine	= init,
+MACHINE_END
