11bebf8124adc14d3ed1c07ee6f1d69ce75552b6
Lu Mao <ylmao@marvell.com>
2011-07-15 16:20:11 +0800

    ARM:pxa:power: add devices power on/off implementation support
    The power on/off of devices should be supported no matter if PM is
    enabled or not.
    Add necessary power on/off functions to support these devices
    workable.
    
    Change-Id: Idf77d6f8466e1d746e369e1ea83a2d08649b7d29
    Signed-off-by: Lu Mao <ylmao@marvell.com>

---
+++ arch/arm/mach-pxa/include/mach/pxa95x_pm.h
@@ -0,0 +1,19 @@
+/*
+ * arch/arm/mach-pxa/include/mach/pxa95x_pm.h
+ *
+ * PXA95x Power Management Routines Head File
+ *
+ * Copyright (C) 2010 Marvell International Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __PXA95X_PM_H__
+#define __PXA95X_PM_H__
+
+#define GC_PWR_ENABLE           (1)
+#define GC_PWR_DISABLE          (0)
+
+#endif
---
+++ arch/arm/mach-pxa/pxa95x_pm.c
@@ -0,0 +1,88 @@
+/*
+ * arch/arm/mach-pxa/include/mach/pxa95x_pm.c
+ *
+ * PXA95x Power Management Routines
+ *
+ * Copyright (C) 2010 Marvell International Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#undef DEBUG
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/err.h>
+#include <linux/time.h>
+#include <linux/delay.h>
+#include <linux/vmalloc.h>
+#include <linux/kobject.h>
+#include <linux/suspend.h>
+#include <linux/clk.h>
+#include <linux/interrupt.h>
+#include <linux/slab.h>
+#include <asm/cacheflush.h>
+#include <asm/io.h>
+#include <asm/mach-types.h>
+#include <mach/hardware.h>
+#include <mach/pxa3xx-regs.h>
+#include <mach/regs-rtc.h>
+#include <mach/regs-intc.h>
+#include <mach/regs-ost.h>
+#include <mach/mfp.h>
+#include <mach/mfp-pxa3xx.h>
+#include <mach/gpio.h>
+#include <mach/pxa95x_pm.h>
+#ifdef CONFIG_ARMV7_OS_SAVE_AND_RESTORE
+#include <asm/hardware/armv7_jtag.h>
+#endif
+
+#define VMETA_PWR_ENABLE        0x1
+#define VMETA_PWR_DISABLE       0x0
+void vmeta_pwr(unsigned int enableDisable)
+{
+	unsigned int vmpwr = 0;
+	vmpwr = VMPWR;
+
+	if (VMETA_PWR_ENABLE == enableDisable) {
+		if (vmpwr & VMPWR_PWR_ST)
+			return;	/*Pwr is already on */
+		VMPWR = VMPWR_SETALLWAYS | VMPWR_PWON;
+		do {
+			vmpwr = VMPWR;
+		} while ((vmpwr & VMPWR_PWR_ST) != VMPWR_PWR_ST);
+	} else if (VMETA_PWR_DISABLE == enableDisable) {
+		if ((vmpwr & VMPWR_PWR_ST) != VMPWR_PWR_ST)
+			return;	/*Pwr is already off */
+		VMPWR = VMPWR_SETALLWAYS;
+	}
+}
+
+void gc_pwr(int enableDisable)
+{
+	unsigned int gcpwr = 0;
+
+	gcpwr = GCPWR;
+	if (GC_PWR_ENABLE == enableDisable) {
+		if (gcpwr & GCPWR_PWR_ST)
+			return;	/*Pwr is already on */
+		GCPWR = GCPWR_SETALLWAYS | GCPWR_PWON;
+		do {
+			gcpwr = GCPWR;
+		} while ((gcpwr & GCPWR_PWR_ST) != GCPWR_PWR_ST);
+		gcpwr = GCPWR;
+		gcpwr |= GCPWR_RST_N;
+		GCPWR = gcpwr;
+	} else if (GC_PWR_DISABLE == enableDisable) {
+		if ((gcpwr & GCPWR_PWR_ST) != GCPWR_PWR_ST)
+			return;	/*Pwr is already off */
+		/* GCPWR_RST_N,GCPWR_PWON = 0 */
+		GCPWR = GCPWR_SETALLWAYS;
+		do {
+			gcpwr = GCPWR;
+		} while ((gcpwr & GCPWR_PWR_ST) == GCPWR_PWR_ST);
+	}
+}
+EXPORT_SYMBOL(gc_pwr);
+
