95e2600545aaf471f2e15af1544c0ef3c58b8a73
Lisa Du <cldu@marvell.com>
2011-09-07 10:33:29 +0800

    ARM:mmp: update dsi & tc35876 settings
    1. Keep dsi lanes in LP-11 state before reset tc358765;
    2. Update tc35876 settings according to dsi 2/4 lanes.
    
    Sync these settings from mmp2 driver.
    
    Change-Id: Id82f42c8a1c0acb79a32a6063bf75186b531276d
    Signed-off-by: Lisa Du <cldu@marvell.com>

--- arch/arm/mach-mmp/onboard/lcd_mipi.c
+++ arch/arm/mach-mmp/onboard/lcd_mipi.c
@@ -75,296 +75,138 @@ static int tc358765_reset(struct pxa168fb_info *fbi)
 	gpio_free(gpio);
 	return 0;
 }
-static int dsi_dump_tc358765(void)
+
+static void tc358765_dump(void)
 {
-	u32 val_32, reg;
-	int ret = 0;
-	/*dump register value*/
-	ret = tc35876x_read32(PPI_TX_RX_TA, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - PPI_TX_RX_TA = 0x%x\n", val_32);
-	else {
-		reg = PPI_TX_RX_TA;
-		goto out_dump;
-	}
+#if 0
+	u32 val;
 
-	mdelay(10);
-	ret = tc35876x_read32(PPI_LPTXTIMECNT, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - PPI_LPTXTIMECNT = 0x%x\n",
-		 val_32);
-	else  {
-		reg = PPI_LPTXTIMECNT;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret  = tc35876x_read32(PPI_D0S_CLRSIPOCOUNT, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - PPI_D0S_CLRSIPOCOUNT = 0x%x\n",
-	       val_32);
-	else {
-		reg = PPI_D0S_CLRSIPOCOUNT;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret  = tc35876x_read32(PPI_D1S_CLRSIPOCOUNT, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - PPI_D1S_CLRSIPOCOUNT = 0x%x\n",
-	       val_32);
-	else {
-		reg = PPI_D1S_CLRSIPOCOUNT;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(PPI_LANEENABLE, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - PPI_LANEENABLE = 0x%x\n", val_32);
-	else {
-		reg = PPI_LANEENABLE;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(DSI_LANEENABLE, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - DSI_LANEENABLE = 0x%x\n", val_32);
-	else {
-		reg = DSI_LANEENABLE;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(PPI_STARTPPI, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - PPI_STARTPPI = 0x%x\n", val_32);
-	else {
-		reg = PPI_STARTPPI;
-		goto out_dump;
-	}
-	ret = tc35876x_read32(DSI_STARTDSI, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - DSI_STARTDSI = 0x%x\n", val_32);
-	else {
-		reg = DSI_STARTDSI;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(VPCTRL, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - VPCTRL = 0x%x\n", val_32);
-	else {
-		reg = VPCTRL;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(HTIM1, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - HTIM1 = 0x%x\n", val_32);
-	else {
-		reg = HTIM1;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(HTIM2, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - HTIM2 = 0x%x\n", val_32);
-	else {
-		reg = HTIM2;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(VTIM1, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - VTIM1 = 0x%x\n", val_32);
-	else {
-		reg = VTIM1;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(VTIM2, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - VTIM2 = 0x%x\n", val_32);
-	else {
-		reg = VTIM2;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(VFUEN, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - VFUEN = 0x%x\n", val_32);
-	else {
-		reg = VFUEN;
-		goto out_dump;
-	}
-	mdelay(10);
-	ret = tc35876x_read32(LVCFG, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "tc35876x - LVCFG = 0x%x\n", val_32);
-	else {
-		reg = LVCFG;
-		goto out_dump;
-	}
-	mdelay(10);
+	pr_info("%s\n", __func__);
+	tc35876x_read32(PPI_TX_RX_TA, &val);
+	pr_info(" - PPI_TX_RX_TA = 0x%x\n", val);
+	tc35876x_read32(PPI_LPTXTIMECNT, &val);
+	pr_info(" - PPI_LPTXTIMECNT = 0x%x\n", val);
+	tc35876x_read32(PPI_D0S_CLRSIPOCOUNT, &val);
+	pr_info(" - PPI_D0S_CLRSIPOCOUNT = 0x%x\n", val);
+	tc35876x_read32(PPI_D1S_CLRSIPOCOUNT, &val);
+	pr_info(" - PPI_D1S_CLRSIPOCOUNT = 0x%x\n", val);
 
+	tc35876x_read32(PPI_D2S_CLRSIPOCOUNT, &val);
+	pr_info(" - PPI_D2S_CLRSIPOCOUNT = 0x%x\n", val);
+	tc35876x_read32(PPI_D3S_CLRSIPOCOUNT, &val);
+	pr_info(" - PPI_D3S_CLRSIPOCOUNT = 0x%x\n", val);
 
-	ret = tc35876x_read32(DSI_INTSTAUS, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "!!tc35876x - DSI_INTSTAUS= 0x%x BEFORE\n",
-	       val_32);
-	else {
-		reg = DSI_INTSTAUS;
-		goto out_dump;
-	}
+	tc35876x_read32(PPI_LANEENABLE, &val);
+	pr_info(" - PPI_LANEENABLE = 0x%x\n", val);
+	tc35876x_read32(DSI_LANEENABLE, &val);
+	pr_info(" - DSI_LANEENABLE = 0x%x\n", val);
+	tc35876x_read32(PPI_STARTPPI, &val);
+	pr_info(" - PPI_STARTPPI = 0x%x\n", val);
+	tc35876x_read32(DSI_STARTDSI, &val);
+	pr_info(" - DSI_STARTDSI = 0x%x\n", val);
 
-	mdelay(10);
-	ret = tc35876x_write32(DSI_INTCLR, 0xFFFFFFFF);
-	if (ret < 0)
-		printk(KERN_DEBUG "tc35876x - write DSI_INTCLR(0x%x) error.\n",
-	       DSI_INTCLR);
-	mdelay(10);
-	ret = tc35876x_read32(DSI_INTSTAUS, &val_32);
-	if (!ret)
-		printk(KERN_DEBUG "!!tc35876x - DSI_INTSTAUS= 0x%x AFTER\n",
-	       val_32);
-	else{
-		reg = DSI_INTSTAUS;
-		goto out_dump;
-	}
-	mdelay(10);
+	tc35876x_read32(VPCTRL, &val);
+	pr_info(" - VPCTRL = 0x%x\n", val);
+	tc35876x_read32(HTIM1, &val);
+	pr_info(" - HTIM1 = 0x%x\n", val);
+	tc35876x_read32(HTIM2, &val);
+	pr_info(" - HTIM2 = 0x%x\n", val);
+	tc35876x_read32(VTIM1, &val);
+	pr_info(" - VTIM1 = 0x%x\n", val);
+	tc35876x_read32(VTIM2, &val);
+	pr_info(" - VTIM2 = 0x%x\n", val);
+	tc35876x_read32(VFUEN, &val);
+	pr_info(" - VFUEN = 0x%x\n", val);
+	tc35876x_read32(LVCFG, &val);
+	pr_info(" - LVCFG = 0x%x\n", val);
 
-out_dump:
-	if (ret < 0)
-		printk(KERN_DEBUG "tc35876x - read reg(0x%x) failed.\n", reg);
-	return ret;
+	tc35876x_read32(DSI_INTSTAUS, &val);
+	pr_info("!! - DSI_INTSTAUS= 0x%x BEFORE\n", val);
+	tc35876x_write32(DSI_INTCLR, 0xFFFFFFFF);
+	tc35876x_read32(DSI_INTSTAUS, &val);
+	pr_info("!! - DSI_INTSTAUS= 0x%x AFTER\n", val);
+
+	tc35876x_read32(DSI_LANESTATUS0, &val);
+	pr_info(" - DSI_LANESTATUS0= 0x%x\n", val);
+	tc35876x_read32(DSIERRCNT, &val);
+	pr_info(" - DSIERRCNT= 0x%x\n", val);
+	tc35876x_read32(DSIERRCNT, &val);
+	pr_info(" - DSIERRCNT= 0x%x AGAIN\n", val);
+	tc35876x_read32(SYSSTAT, &val);
+	pr_info(" - SYSSTAT= 0x%x\n", val);
+#endif
 }
 
 static int dsi_set_tc358765(struct pxa168fb_info *fbi)
 {
-	int status = 0;
+	int status;
 #ifdef CONFIG_TC35876X
+	struct fb_var_screeninfo *var = &(fbi->fb_info->var);
+	struct pxa168fb_mach_info *mi = fbi->dev->platform_data;
+	struct dsi_info *di = mi->dsi;
 	u16 chip_id = 0;
-	int reg;
 
 	status = tc35876x_read16(TC358765_CHIPID_REG, &chip_id);
 	if ((status < 0) || (chip_id != TC358765_CHIPID)) {
-		printk(KERN_WARNING "tc35876x unavailable! chip_id %x\n",
-		       chip_id);
+		pr_err("tc35876x unavailable! chip_id %x\n", chip_id);
 		return -EIO;
-	} else {
-		printk(KERN_INFO "tc35876x(chip id:0x%02x) detected.\n",
-		       chip_id);
-	}
-	/* REG 0x13C,DAT 0x000C000F*/
-	status = tc35876x_write32(PPI_TX_RX_TA, 0x000C000F);
-	if (status < 0) {
-		reg = PPI_TX_RX_TA;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x114,DAT 0x0000000A*/
-	status = tc35876x_write32(PPI_LPTXTIMECNT, 0x0000000A);
-	if (status < 0) {
-		reg = PPI_LPTXTIMECNT;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x164,DAT 0x00000005*/
-	status = tc35876x_write32(PPI_D0S_CLRSIPOCOUNT, 0x00000005);
-	if (status < 0) {
-		reg = PPI_D0S_CLRSIPOCOUNT;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x168,DAT 0x00000005*/
-	status = tc35876x_write32(PPI_D1S_CLRSIPOCOUNT, 0x00000005);
-	if (status < 0) {
-		reg = PPI_D1S_CLRSIPOCOUNT;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x134,DAT 0x00000007*/
-	status = tc35876x_write32(PPI_LANEENABLE, 0x0000007);
-	if (status < 0) {
-		reg = PPI_LANEENABLE;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x210,DAT 0x00000007*/
-	status = tc35876x_write32(DSI_LANEENABLE, 0x0000007);
-	if (status < 0) {
-		reg = DSI_LANEENABLE;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x104,DAT 0x00000001*/
-	status = tc35876x_write32(PPI_STARTPPI, 0x0000001);
-	if (status < 0) {
-		reg = PPI_STARTPPI;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x204,DAT 0x00000001*/
-	status = tc35876x_write32(DSI_STARTDSI, 0x0000001);
-	if (status < 0) {
-		reg = DSI_STARTDSI;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x450,DAT 0x00012020, bit 4 is VTGEN on*/
-	status = tc35876x_write32(VPCTRL, 0x00012020);
-	if (status < 0) {
-		reg = VPCTRL;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x454,DAT 0x000A0002*/
-	status = tc35876x_write32(HTIM1, 0x000A0002);
-	if (status < 0) {
-		reg = HTIM1;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x458,DAT 0x00740500*/
-	status = tc35876x_write32(HTIM2, 0x00740500);
-	if (status < 0) {
-		reg = HTIM2;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x45C,DAT 0x000A0004*/
-	status = tc35876x_write32(VTIM1, 0x000A0004);
-	if (status < 0) {
-		reg = VTIM1;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x460,DAT 0x000402D0*/
-	status = tc35876x_write32(VTIM2, 0x000402D0);
-	if (status < 0) {
-		reg = VTIM2;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x464,DAT 0x00000001*/
-	status = tc35876x_write32(VFUEN, 0x00000001);
-	if (status < 0) {
-		reg = VFUEN;
-		goto out_set;
-	}
-	mdelay(10);
-	/*REG 0x49C,DAT 0x00000201*/
-	status = tc35876x_write32(LVCFG, 0x00000201);
-	if (status < 0) {
-		reg = LVCFG;
-		goto out_set;
-	}
-	mdelay(10);
+	} else
+		pr_debug("tc35876x(chip id:0x%02x) detected.\n", chip_id);
 
-	status = dsi_dump_tc358765();
-	return status;
-out_set:
-	if (status < 0)
-		printk(KERN_WARNING "tc35876x write reg(0x%x) failed.\n", reg);
+	/* REG 0x13C,DAT 0x000C000F */
+	tc35876x_write32(PPI_TX_RX_TA, 0x00040004);
+	/* REG 0x114,DAT 0x0000000A */
+	tc35876x_write32(PPI_LPTXTIMECNT, 0x00000004);
+
+	/* get middle value of mim-max value
+	 * 0-0x13 for 2lanes-rgb888, 0-0x26 for 4lanes-rgb888
+	 * 0-0x21 for 2lanes-rgb565, 0-0x25 for 4lanes-rgb565
+	 */
+	if (di->lanes == 4)
+		status = 0x13;
+	else if (di->bpp == 24)
+		status = 0xa;
+	else
+		status = 0x11;
+	/* REG 0x164,DAT 0x00000005 */
+	tc35876x_write32(PPI_D0S_CLRSIPOCOUNT, status);
+	/* REG 0x168,DAT 0x00000005 */
+	tc35876x_write32(PPI_D1S_CLRSIPOCOUNT, status);
+	if (di->lanes == 4) {
+		/* REG 0x16C,DAT 0x00000005 */
+		tc35876x_write32(PPI_D2S_CLRSIPOCOUNT, status);
+		/* REG 0x170,DAT 0x00000005 */
+		tc35876x_write32(PPI_D3S_CLRSIPOCOUNT, status);
+	}
+
+	/* REG 0x134,DAT 0x00000007 */
+	tc35876x_write32(PPI_LANEENABLE, (di->lanes == 4) ? 0x1f : 0x7);
+	/* REG 0x210,DAT 0x00000007 */
+	tc35876x_write32(DSI_LANEENABLE, (di->lanes == 4) ? 0x1f : 0x7);
+
+	/* REG 0x104,DAT 0x00000001 */
+	tc35876x_write32(PPI_STARTPPI, 0x0000001);
+	/* REG 0x204,DAT 0x00000001 */
+	tc35876x_write32(DSI_STARTDSI, 0x0000001);
+
+	/* REG 0x450,DAT 0x00012020, VSDELAY = 8 pixels */
+	tc35876x_write32(VPCTRL, 0x00800020);
+
+	/* REG 0x454,DAT 0x00200008*/
+	tc35876x_write32(HTIM1, ((var->left_margin) << 16)
+			| var->hsync_len);
+
+	/* REG 0x45C,DAT 0x00040004*/
+	tc35876x_write32(VTIM1, ((var->upper_margin) << 16)
+			| var->vsync_len);
+
+	/* REG 0x49C,DAT 0x00000201 */
+	tc35876x_write32(LVCFG, 0x00000001);
+
+	/* dump register value */
+	tc358765_dump();
 #endif
-	return status;
+	return 0;
 }
 
 static int abilene_lcd_power(struct pxa168fb_info *fbi,
@@ -433,7 +275,7 @@ static int dsi_init(struct pxa168fb_info *fbi)
 {
 #ifdef CONFIG_PXA688_DSI
 	struct pxa168fb_mach_info *mi = fbi->dev->platform_data;
-	int sclk, ret = 0;
+	int ret = 0;
 
 	/* reset DSI controller */
 	dsi_reset(fbi, 1);
@@ -442,38 +284,33 @@ static int dsi_init(struct pxa168fb_info *fbi)
 	/* disable continuous clock */
 	dsi_cclk_set(fbi, 0);
 
-	/*  reset the bridge */
-	if (mi->xcvr_reset)
-		mi->xcvr_reset(fbi);
-
 	/* dsi out of reset */
 	dsi_reset(fbi, 0);
 
 	/* set dphy */
 	dsi_set_dphy(fbi);
 
+	/* put all lanes to LP-11 state  */
+	dsi_lanes_enable(fbi, 0);
+	dsi_lanes_enable(fbi, 1);
+
+	/*  reset the bridge */
+	if (mi->xcvr_reset)
+		mi->xcvr_reset(fbi);
+
+	mdelay(10);
 	/* set dsi controller */
 	dsi_set_controller(fbi);
 
 	/* turn on DSI continuous clock */
 	dsi_cclk_set(fbi, 1);
 
-	/* XM: save original sclk divider & set to 6
-	 * divider 6 is a magic number to help adjust phase.
-	 * ESC clk = core clk /12 = 800Mhz/12 = 66Mhz. */
-	sclk = sclk_div_get(fbi->id);
-	sclk_div_set(fbi->id, 6);
-
 	/* set dsi to dpi conversion chip */
 	if (mi->phy_type == DSI2DPI) {
 		ret = mi->dsi2dpi_set(fbi);
 		if (ret < 0)
-			printk(KERN_WARNING "dsi2dpi_set error!\n");
+			pr_err("dsi2dpi_set error!\n");
 	}
-
-	/* restore sclk div */
-	sclk_div_set(fbi->id, sclk);
-	msleep(1);
 #endif
 	return 0;
 }
