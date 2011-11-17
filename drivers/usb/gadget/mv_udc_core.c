25fbb0d6ac7e2e6e04272c50d3c620c9cfe641cd
Neil Zhang <zhangwm@marvell.com>
2011-11-17 16:49:23 +0800

    usb: gadget: mv_udc: notify vbus charger ASAP
    In order to notify vbus charger ASAP, we'd better notify it when
    configuration finished, but we need not to notify it all the time when
    we are doing USB CV Enumeration Test.
    
    Change-Id: I2cafe51b1d040868f9a12b724c357bbf1523285c
    Signed-off-by: Neil Zhang <zhangwm@marvell.com>

--- drivers/usb/gadget/mv_udc_core.c
+++ drivers/usb/gadget/mv_udc_core.c
@@ -62,7 +62,7 @@ static int	dvfm_dev_idx;
 #define LOOPS_USEC		(1 << LOOPS_USEC_SHIFT)
 #define LOOPS(timeout)		((timeout) >> LOOPS_USEC_SHIFT)
 
-#define	ENUMERATION_DELAY	(6 * HZ)
+#define	ENUMERATION_DELAY	(10 * HZ)
 
 static const char driver_name[] = "mv_udc";
 static const char driver_desc[] = DRIVER_DESC;
@@ -1870,8 +1870,11 @@ static void handle_setup_packet(struct mv_udc *udc, u8 ep_num,
 		}
 
 		if (is_set_configuration(setup)) {
-			wake_lock(&idle_lock);
-			udc->charger_type = VBUS_CHARGER;
+			if (udc->charger_type == DEFAULT_CHARGER) {
+				wake_lock(&idle_lock);
+				udc->charger_type = VBUS_CHARGER;
+				schedule_delayed_work(&udc->charger_work, 0);
+			}
 		}
 	}
 }
@@ -2241,16 +2244,13 @@ static void do_charger_work(struct work_struct *work)
 
 	udc = container_of(work, struct mv_udc, charger_work.work);
 
-	if (udc->charger_type != VBUS_CHARGER
-		&& udc->charger_type != NULL_CHARGER) {
+	if (udc->charger_type == DEFAULT_CHARGER) {
 		val = readl(&udc->op_regs->portsc) & charger_mask;
 		if (val == charger_mask)
 			udc->charger_type = AC_CHARGER_STANDARD;
-		else {
-			udc->charger_type = DEFAULT_CHARGER;
+		else
 			schedule_delayed_work(&udc->delayed_charger_work,
 						ENUMERATION_DELAY);
-		}
 	}
 
 	call_charger_notifier(udc);
@@ -2262,10 +2262,10 @@ static void do_delayed_work(struct work_struct *work)
 
 	udc = container_of(work, struct mv_udc, delayed_charger_work.work);
 
-	if (udc->charger_type == DEFAULT_CHARGER)
+	if (udc->charger_type == DEFAULT_CHARGER) {
 		udc->charger_type = AC_CHARGER_OTHER;
-
-	call_charger_notifier(udc);
+		call_charger_notifier(udc);
+	}
 }
 
 static irqreturn_t mv_udc_vbus_irq(int irq, void *dev)
