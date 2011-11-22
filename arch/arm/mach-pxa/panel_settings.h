e6c3132625aca445c5173a49bcb33e1c324b094b
Xiaofan Tian <tianxf@marvell.com>
2011-11-22 13:00:57 +0800

    ARM: pxa: add truly WVGA 1P3624 panel setting
    Change-Id: Ife7a55e7a7b14b2df34cb402a7a3e38003f93009
    Signed-off-by: Xiaofan Tian <tianxf@marvell.com>

--- arch/arm/mach-pxa/panel_settings.h
+++ arch/arm/mach-pxa/panel_settings.h
@@ -5,6 +5,7 @@
 
 extern struct fb_videomode video_modes_tc3587[1];
 extern struct fb_videomode video_modes_trulywvga[1];
+extern struct fb_videomode video_modes_truly1p3624[1];
 extern struct fb_videomode video_modes_si9226[1];
 extern struct fb_videomode video_modes_adv7533[1];
 extern struct fb_videomode video_modes_ihdmi[MV_IHDMI_MODES];
@@ -12,5 +13,6 @@ extern struct fb_videomode video_modes_ihdmi[MV_IHDMI_MODES];
 void panel_power_tc3587(int ssp_port, int on);
 void panel_power_trulywvga(int ssp_port, int on);
 void panel_set_trulywvga(int ssp_port);
+void panel_power_truly1p3624(int ssp_port, int on);
 
 #endif
