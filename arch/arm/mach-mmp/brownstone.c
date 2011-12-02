09de5dc50ec4897be70f34f6cf73088452fb69e7
zhaoy <zhaoy@marvell.com>
2011-12-02 10:30:54 +0800

    Audio:add arch file for headset switch for brownstone
    	1. add headset switch arch file for brownstone
    
    Change-Id: I1a87bc643f0d173d4cb907d6ddc2371ea5054ffe
    Signed-off-by: zhaoy <zhaoy@marvell.com>

--- arch/arm/mach-mmp/brownstone.c
+++ arch/arm/mach-mmp/brownstone.c
@@ -23,6 +23,7 @@
 #include <linux/pwm_backlight.h>
 #include <linux/fb.h>
 #include <linux/delay.h>
+#include <linux/switch.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <mach/addr-map.h>
@@ -1075,6 +1076,45 @@ static void __init mmp2_init_spi(void)
 static inline void mmp2_init_spi(void) {}
 #endif
 
+#if defined(CONFIG_SWITCH_HEADSET_HOST_GPIO)
+static struct gpio_switch_platform_data headset_switch_device_data = {
+	.name = "h2w",
+	.gpio = mfp_to_gpio(GPIO23_GPIO),
+	.name_on = NULL,
+	.name_off = NULL,
+	.state_on = NULL,
+	.state_off = NULL,
+};
+
+static struct platform_device headset_switch_device = {
+	.name            = "headset",
+	.id              = 0,
+	.dev             = {
+		.platform_data = &headset_switch_device_data,
+	},
+};
+
+static int wm8994_gpio_irq(void)
+{
+	int gpio = mfp_to_gpio(GPIO23_GPIO);
+
+	if (gpio_request(gpio, "wm8994 irq")) {
+		printk(KERN_INFO "gpio %d request failed\n", gpio);
+		return -1;
+	}
+
+	gpio_direction_input(gpio);
+	mdelay(1);
+	gpio_free(gpio);
+	return 0;
+}
+
+static void __init brownstone_init_headset(void)
+{
+	wm8994_gpio_irq();
+	platform_device_register(&headset_switch_device);
+}
+#endif
 
 #ifdef CONFIG_UIO_HDMI
 static struct uio_hdmi_platform_data mmp2_hdmi_info __initdata = {
@@ -1349,6 +1389,10 @@ static void __init brownstone_init(void)
 
 #ifdef CONFIG_UIO_HDMI
 	mmp2_add_hdmi(&mmp2_hdmi_info);
+#endif
+
+#ifdef CONFIG_SWITCH_HEADSET_HOST_GPIO
+	brownstone_init_headset();
 #endif
 
 #ifdef CONFIG_ANDROID_PMEM
