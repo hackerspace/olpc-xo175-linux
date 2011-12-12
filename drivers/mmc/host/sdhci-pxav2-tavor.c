8ca9f6a5c8ff020ae3606fe834d73e0e8b5f1683
Raymond Wu <xywu@marvell.com>
2011-12-12 17:27:12 +0800

    mmc: remove the lp_switch for sdhci-tavor controller
    in sdhci.c, we will power off the ldo by the open source code.
    
    Change-Id: I8e8602f6b8fc446bc25da13c640ce82c7577ab17
    Signed-off-by: Raymond Wu <xywu@marvell.com>

--- drivers/mmc/host/sdhci-pxav2-tavor.c
+++ drivers/mmc/host/sdhci-pxav2-tavor.c
@@ -401,7 +401,6 @@ static int __devexit sdhci_pxav2_remove(struct platform_device *pdev)
 static int sdhci_pxav2_suspend(struct platform_device *pdev, pm_message_t state)
 {
 	struct sdhci_host *host = platform_get_drvdata(pdev);
-	struct sdhci_pxa_platdata *pdata = pdev->dev.platform_data;
 	int ret = 0;
 
 	/* Skip the suspend process if the controller is to be accessed during suspend */
@@ -417,15 +416,7 @@ static int sdhci_pxav2_suspend(struct platform_device *pdev, pm_message_t state)
 	if (ret)
 		return ret;
 
-	if (pdata->lp_switch) {
-		ret = pdata->lp_switch(1, (int)host->mmc->card);
-		if (ret) {
-			sdhci_resume_host(host);
-			dev_err(&pdev->dev, "fail to switch gpio, resume..\n");
-		}
-	}
-	if (!ret)
-		host->mmc->suspended = 1;
+	host->mmc->suspended = 1;
 
 	return ret;
 }
@@ -433,7 +424,6 @@ static int sdhci_pxav2_suspend(struct platform_device *pdev, pm_message_t state)
 static int sdhci_pxav2_resume(struct platform_device *pdev)
 {
 	struct sdhci_host *host = platform_get_drvdata(pdev);
-	struct sdhci_pxa_platdata *pdata = pdev->dev.platform_data;
 	int ret = 0;
 
 	/* Skip the resume process if the controller is to be accessed during suspend */
@@ -441,9 +431,6 @@ static int sdhci_pxav2_resume(struct platform_device *pdev)
 	if(host->mmc->pm_flags & MMC_PM_ALWAYS_ACTIVE) {
 		return ret;
 	}
-
-	if (pdata->lp_switch)
-		pdata->lp_switch(0, (int)host->mmc->card);
 
 	host->mmc->suspended = 0;
 	ret = sdhci_resume_host(host);
