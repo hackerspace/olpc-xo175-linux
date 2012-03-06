d17f58fcef2abe0940464c5e2946888eac0617e6
Yunfan Zhang <yfzhang@marvell.com>
2012-03-06 17:13:52 +0800

    ARM: mmp: add reboot and poweroff support for Orchid
    Change-Id: I215926fb9ee2a12c15a0571c1082eec0f570cc47
    Signed-off-by: Yunfan Zhang <yfzhang@marvell.com>

--- arch/arm/mach-mmp/orchid.c
+++ arch/arm/mach-mmp/orchid.c
@@ -1025,16 +1025,235 @@ static inline void mmp3_init_spi(void) {
 }
 #endif
 
+/* Only for reboot routine */
+static const u32 reg_idbr = APB_VIRT_BASE + 0x11000 + 0x08;
+static const u32 reg_icr = APB_VIRT_BASE + 0x11000 + 0x10;
+static const u32 reg_isr = APB_VIRT_BASE + 0x11000 + 0x18;
+
+/* Control register bits */
+#define ICR_START	(1 << 0)
+#define ICR_STOP	(1 << 1)
+#define ICR_ACKNAK	(1 << 2)
+#define ICR_TB		(1 << 3)
+#define ICR_ALDIE	(1 << 12)
+/* Status register bits */
+#define ISR_ACKNAK	(1 << 1)
+#define ISR_IBB		(1 << 3)
+#define ISR_ITE		(1 << 6)
+#define ISR_IRF		(1 << 7)
+
+static int i2c_isr_set_cleared(u32 set, u32 cleared)
+{
+	int isr, timeout = 1000;
+	do {
+		isr = readl(reg_isr);
+		udelay(10);
+		if (timeout-- < 0)
+			return 0;
+	} while (((isr & set) != set) ||
+			((isr & cleared) != 0));
+	return 1;
+}
+
+static int pm800_i2c_write_reg(u8 addr, u8 reg, u8 val)
+{
+	/* Is bus busy? */
+	if (!i2c_isr_set_cleared(0, ISR_IBB))
+		return -1;
+	/* -----1. Send chip addr -------  */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write chip addr */
+	writel((addr << 1), reg_idbr);
+	/* Send start bit */
+	writel(readl(reg_icr) | ICR_START, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
+		return -1;
+	/* -----2. Send reg addr ------ */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write reg addr */
+	writel(reg, reg_idbr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
+		return -1;
+	/* -----3. Send val ------ */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write val */
+	writel(val, reg_idbr);
+	/* Send Stop bit */
+	writel(readl(reg_icr) | ICR_STOP, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
+		return -1;
+
+	return 0;
+}
+
+static int pm800_i2c_read_reg(u8 addr, u8 reg, u8 *buf, int len)
+{
+	/* Is bus busy? */
+	if (!i2c_isr_set_cleared(0, ISR_IBB))
+		return -1;
+	/* -----1. Send chip addr -------  */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write chip addr */
+	writel((addr << 1), reg_idbr);
+	/* Send start bit */
+	writel(readl(reg_icr) | ICR_START, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
+		return -1;
+	/* -----2. Send reg addr ------ */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write reg addr */
+	writel(reg, reg_idbr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
+		return -1;
+	/* -----3. Start read sequence -------  */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write chip addr: R/nW=1 */
+	writel((addr << 1) | 0x1, reg_idbr);
+	/* Send start bit */
+	writel(readl(reg_icr) | ICR_START, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
+		return -1;
+	while (len--) {
+		/* Clear START and STOP bits */
+		writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+		writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+		if (len == 0)
+			/* Send NACK */
+			writel(readl(reg_icr) | ICR_ACKNAK, reg_icr);
+		else
+			/* Send ACK */
+			writel(readl(reg_icr) & ~ICR_ACKNAK, reg_icr);
+		/* Clear ALDIE */
+		writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+		/* Transfer, receive a byte */
+		writel(readl(reg_icr) | ICR_TB, reg_icr);
+		/* Receive register full? */
+		if (!i2c_isr_set_cleared(ISR_IRF, 0))
+			return -1;
+		*buf = readl(reg_idbr);
+		buf++;
+		/* Clear ITE: W1C */
+		writel(readl(reg_isr) | ISR_IRF, reg_isr);
+	}
+	return 0;
+}
+
 static int orchid_board_reset(char mode, const char *cmd)
 {
-	/* TODO: add board reset routine */
+	u8 buf[4];
+	u32 ticks;
+	/* Reset TWSI1 unit firstly */
+	writel(0x4060, reg_icr);
+	udelay(500);
+	writel(0x60, reg_icr);
+	udelay(500);
+	/* 1. Disable alarm1 */
+	pm800_i2c_write_reg(0x30, 0xD0, 0x80);
+	/* 2. Read RTC counter */
+	pm800_i2c_read_reg(0x30, 0xD1, buf, 4);
+	ticks = (buf[3] << 24) | (buf[2] << 16) |
+			(buf[1] << 8) | buf[0];
+	/* 3.Calculate alarm expire counter: after 1s here */
+	ticks += 1;
+	buf[0] = ticks & 0xFF;
+	buf[1] = (ticks >> 8) & 0xFF;
+	buf[2] = (ticks >> 16) & 0xFF;
+	buf[3] = (ticks >> 24) & 0xFF;
+	/* 4.Set alarm1 */
+	pm800_i2c_write_reg(0x30, 0xD5, buf[0]);
+	pm800_i2c_write_reg(0x30, 0xD6, buf[1]);
+	pm800_i2c_write_reg(0x30, 0xD7, buf[2]);
+	pm800_i2c_write_reg(0x30, 0xD8, buf[3]);
+	/* 5.Use XO and enable alarm1 */
+	pm800_i2c_write_reg(0x30, 0xD0, 0x81);
+	/* 6.Issue SW power down */
+	pm800_i2c_write_reg(0x30, 0x0D, 0x20);
+	/* Rebooting... */
 	return 1;
+}
+
+static void orchid_poweroff(void)
+{
+	pm8xxx_system_poweroff();
 }
 
 static void __init orchid_init(void)
 {
 	extern int (*board_reset)(char mode, const char *cmd);
 	board_reset = orchid_board_reset;
+	pm_power_off = orchid_poweroff;
 	mfp_config(ARRAY_AND_SIZE(orchid_pin_config));
 
 	/* on-chip devices */
