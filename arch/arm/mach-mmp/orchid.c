cc3b0d6aaed56552514b7ba9eea48a7a11abe14d
Yunfan Zhang <yfzhang@marvell.com>
2012-03-13 19:25:31 +0800

    ARM: mmp: add raw i2c operation support for reset
    Add raw i2c APIs can only be used at rebooting stage:
    int __raw_i2c_bus_reset(u8 bus_num);
    int __raw_i2c_write_reg(u8 bus_num, u8 addr, u8 reg, u8 val);
    int __raw_i2c_read_reg(u8 bus_num, u8 addr, u8 reg, u8 *buf, int len);
    
    Change-Id: Ifb3991caf89eaaa5ed983caeccfa5ddb7681709d
    Signed-off-by: Yunfan Zhang <yfzhang@marvell.com>

--- arch/arm/mach-mmp/orchid.c
+++ arch/arm/mach-mmp/orchid.c
@@ -870,198 +870,24 @@ static void __init mmp_init_ap_cp(void)
 }
 
 /* Only for reboot routine */
-static const u32 reg_idbr = APB_VIRT_BASE + 0x11000 + 0x08;
-static const u32 reg_icr = APB_VIRT_BASE + 0x11000 + 0x10;
-static const u32 reg_isr = APB_VIRT_BASE + 0x11000 + 0x18;
-
-/* Control register bits */
-#define ICR_START	(1 << 0)
-#define ICR_STOP	(1 << 1)
-#define ICR_ACKNAK	(1 << 2)
-#define ICR_TB		(1 << 3)
-#define ICR_ALDIE	(1 << 12)
-/* Status register bits */
-#define ISR_ACKNAK	(1 << 1)
-#define ISR_IBB		(1 << 3)
-#define ISR_ITE		(1 << 6)
-#define ISR_IRF		(1 << 7)
-
-static int i2c_isr_set_cleared(u32 set, u32 cleared)
-{
-	int isr, timeout = 1000;
-	do {
-		isr = readl(reg_isr);
-		udelay(10);
-		if (timeout-- < 0)
-			return 0;
-	} while (((isr & set) != set) ||
-			((isr & cleared) != 0));
-	return 1;
-}
-
+extern int __raw_i2c_bus_reset(u8 bus_num);
+extern int __raw_i2c_write_reg(u8 bus_num, u8 addr, u8 reg, u8 val);
+extern int __raw_i2c_read_reg(u8 bus_num, u8 addr, u8 reg, u8 *buf, int len);
 static int pm800_i2c_write_reg(u8 addr, u8 reg, u8 val)
 {
-	/* Is bus busy? */
-	if (!i2c_isr_set_cleared(0, ISR_IBB))
-		return -1;
-	/* -----1. Send chip addr -------  */
-	/* Clear START and STOP bits */
-	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-	/* Write chip addr */
-	writel((addr << 1), reg_idbr);
-	/* Send start bit */
-	writel(readl(reg_icr) | ICR_START, reg_icr);
-	/* Clear ALDIE */
-	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-	/* Transfer, send a byte */
-	writel(readl(reg_icr) | ICR_TB, reg_icr);
-	/* Transmit empty? */
-	if (!i2c_isr_set_cleared(ISR_ITE, 0))
-		return -1;
-	/* Clear ITE: W1C */
-	writel(readl(reg_isr) | ISR_ITE, reg_isr);
-	/* Wait for ACK */
-	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
-		return -1;
-	/* -----2. Send reg addr ------ */
-	/* Clear START and STOP bits */
-	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-	/* Write reg addr */
-	writel(reg, reg_idbr);
-	/* Clear ALDIE */
-	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-	/* Transfer, send a byte */
-	writel(readl(reg_icr) | ICR_TB, reg_icr);
-	/* Transmit empty? */
-	if (!i2c_isr_set_cleared(ISR_ITE, 0))
-		return -1;
-	/* Clear ITE: W1C */
-	writel(readl(reg_isr) | ISR_ITE, reg_isr);
-	/* Wait for ACK */
-	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
-		return -1;
-	/* -----3. Send val ------ */
-	/* Clear START and STOP bits */
-	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-	/* Write val */
-	writel(val, reg_idbr);
-	/* Send Stop bit */
-	writel(readl(reg_icr) | ICR_STOP, reg_icr);
-	/* Clear ALDIE */
-	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-	/* Transfer, send a byte */
-	writel(readl(reg_icr) | ICR_TB, reg_icr);
-	/* Transmit empty? */
-	if (!i2c_isr_set_cleared(ISR_ITE, 0))
-		return -1;
-	/* Clear ITE: W1C */
-	writel(readl(reg_isr) | ISR_ITE, reg_isr);
-	/* Wait for ACK */
-	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
-		return -1;
-
-	return 0;
+	return __raw_i2c_write_reg(1, addr, reg, val);
 }
 
 static int pm800_i2c_read_reg(u8 addr, u8 reg, u8 *buf, int len)
 {
-	/* Is bus busy? */
-	if (!i2c_isr_set_cleared(0, ISR_IBB))
-		return -1;
-	/* -----1. Send chip addr -------  */
-	/* Clear START and STOP bits */
-	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-	/* Write chip addr */
-	writel((addr << 1), reg_idbr);
-	/* Send start bit */
-	writel(readl(reg_icr) | ICR_START, reg_icr);
-	/* Clear ALDIE */
-	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-	/* Transfer, send a byte */
-	writel(readl(reg_icr) | ICR_TB, reg_icr);
-	/* Transmit empty? */
-	if (!i2c_isr_set_cleared(ISR_ITE, 0))
-		return -1;
-	/* Clear ITE: W1C */
-	writel(readl(reg_isr) | ISR_ITE, reg_isr);
-	/* Wait for ACK */
-	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
-		return -1;
-	/* -----2. Send reg addr ------ */
-	/* Clear START and STOP bits */
-	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-	/* Write reg addr */
-	writel(reg, reg_idbr);
-	/* Clear ALDIE */
-	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-	/* Transfer, send a byte */
-	writel(readl(reg_icr) | ICR_TB, reg_icr);
-	/* Transmit empty? */
-	if (!i2c_isr_set_cleared(ISR_ITE, 0))
-		return -1;
-	/* Clear ITE: W1C */
-	writel(readl(reg_isr) | ISR_ITE, reg_isr);
-	/* Wait for ACK */
-	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
-		return -1;
-	/* -----3. Start read sequence -------  */
-	/* Clear START and STOP bits */
-	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-	/* Write chip addr: R/nW=1 */
-	writel((addr << 1) | 0x1, reg_idbr);
-	/* Send start bit */
-	writel(readl(reg_icr) | ICR_START, reg_icr);
-	/* Clear ALDIE */
-	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-	/* Transfer, send a byte */
-	writel(readl(reg_icr) | ICR_TB, reg_icr);
-	/* Transmit empty? */
-	if (!i2c_isr_set_cleared(ISR_ITE, 0))
-		return -1;
-	/* Clear ITE: W1C */
-	writel(readl(reg_isr) | ISR_ITE, reg_isr);
-	/* Wait for ACK */
-	if (!i2c_isr_set_cleared(0, ISR_ACKNAK))
-		return -1;
-	while (len--) {
-		/* Clear START and STOP bits */
-		writel(readl(reg_icr) & ~(ICR_START), reg_icr);
-		writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
-		if (len == 0)
-			/* Send NACK */
-			writel(readl(reg_icr) | ICR_ACKNAK, reg_icr);
-		else
-			/* Send ACK */
-			writel(readl(reg_icr) & ~ICR_ACKNAK, reg_icr);
-		/* Clear ALDIE */
-		writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
-		/* Transfer, receive a byte */
-		writel(readl(reg_icr) | ICR_TB, reg_icr);
-		/* Receive register full? */
-		if (!i2c_isr_set_cleared(ISR_IRF, 0))
-			return -1;
-		*buf = readl(reg_idbr);
-		buf++;
-		/* Clear ITE: W1C */
-		writel(readl(reg_isr) | ISR_IRF, reg_isr);
-	}
-	return 0;
+	return __raw_i2c_read_reg(1, addr, reg, buf, len);
 }
 
 static int orchid_board_reset(char mode, const char *cmd)
 {
 	u8 buf;
 	/* Reset TWSI1 unit firstly */
-	writel(0x4060, reg_icr);
-	udelay(500);
-	writel(0x60, reg_icr);
-	udelay(500);
+	__raw_i2c_bus_reset(1);
 	/* 1.Enable FAULT_WU and FAULT_WU_EN */
 	pm800_i2c_read_reg(0x30, 0xE7, &buf, 1);
 	buf |= ((1 << 3) | (1 << 2));
--- arch/arm/mach-mmp/reset.c
+++ arch/arm/mach-mmp/reset.c
@@ -18,6 +18,270 @@
 #include <linux/delay.h>
 #include <linux/mfd/88pm860x.h>
 
+/* Raw i2c operations __ONLY__ can be used for reboot routine */
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)	\
+		|| defined(CONFIG_CPU_PXA910)
+
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
+#define MAX_BUS_NUM	6
+static const u32 i2c_reg_idbr[MAX_BUS_NUM] = {
+	APB_VIRT_BASE + 0x11000 + 0x08,
+	APB_VIRT_BASE + 0x31000 + 0x08,
+	APB_VIRT_BASE + 0x32000 + 0x08,
+	APB_VIRT_BASE + 0x33000 + 0x08,
+	APB_VIRT_BASE + 0x33800 + 0x08,
+	APB_VIRT_BASE + 0x34000 + 0x08,
+};
+static const u32 i2c_reg_icr[MAX_BUS_NUM] = {
+	APB_VIRT_BASE + 0x11000 + 0x10,
+	APB_VIRT_BASE + 0x31000 + 0x10,
+	APB_VIRT_BASE + 0x32000 + 0x10,
+	APB_VIRT_BASE + 0x33000 + 0x10,
+	APB_VIRT_BASE + 0x33800 + 0x10,
+	APB_VIRT_BASE + 0x34000 + 0x10,
+};
+static const u32 i2c_reg_isr[MAX_BUS_NUM] = {
+	APB_VIRT_BASE + 0x11000 + 0x18,
+	APB_VIRT_BASE + 0x31000 + 0x18,
+	APB_VIRT_BASE + 0x32000 + 0x18,
+	APB_VIRT_BASE + 0x33000 + 0x18,
+	APB_VIRT_BASE + 0x33800 + 0x18,
+	APB_VIRT_BASE + 0x34000 + 0x18,
+};
+#endif
+
+#if defined(CONFIG_CPU_PXA910)
+#define MAX_BUS_NUM	2
+static const u32 i2c_reg_idbr[MAX_BUS_NUM] = {
+	APB_VIRT_BASE + 0x11000 + 0x08,
+	APB_VIRT_BASE + 0x37000 + 0x08,
+};
+static const u32 i2c_reg_icr[MAX_BUS_NUM] = {
+	APB_VIRT_BASE + 0x11000 + 0x10,
+	APB_VIRT_BASE + 0x37000 + 0x10,
+};
+static const u32 i2c_reg_isr[MAX_BUS_NUM] = {
+	APB_VIRT_BASE + 0x11000 + 0x18,
+	APB_VIRT_BASE + 0x37000 + 0x18,
+};
+#endif
+
+/* Control register bits */
+#define ICR_START	(1 << 0)
+#define ICR_STOP	(1 << 1)
+#define ICR_ACKNAK	(1 << 2)
+#define ICR_TB		(1 << 3)
+#define ICR_ALDIE	(1 << 12)
+/* Status register bits */
+#define ISR_ACKNAK	(1 << 1)
+#define ISR_IBB		(1 << 3)
+#define ISR_ITE		(1 << 6)
+#define ISR_IRF		(1 << 7)
+
+static int i2c_isr_set_cleared(u32 reg_isr, u32 set, u32 cleared)
+{
+	int isr, timeout = 1000;
+	do {
+		udelay(10);
+		isr = readl(reg_isr);
+		if (timeout-- < 0)
+			return 0;
+	} while (((isr & set) != set) ||
+			((isr & cleared) != 0));
+	return 1;
+}
+
+int __raw_i2c_bus_reset(u8 bus_num)
+{
+	u32 reg_icr;
+	if (bus_num == 0 || bus_num > MAX_BUS_NUM) {
+		pr_err("%s: bus_num if out of range!\n", __func__);
+		return -1;
+	}
+	reg_icr = i2c_reg_icr[bus_num - 1];
+	writel(0x4060, reg_icr);
+	udelay(500);
+	writel(0x60, reg_icr);
+	udelay(500);
+	return 0;
+}
+
+int __raw_i2c_write_reg(u8 bus_num, u8 addr, u8 reg, u8 val)
+{
+	u32 reg_idbr, reg_icr, reg_isr;
+	if (bus_num == 0 || bus_num > MAX_BUS_NUM) {
+		pr_err("%s: bus_num if out of range!\n", __func__);
+		return -1;
+	}
+	reg_idbr = i2c_reg_idbr[bus_num - 1];
+	reg_icr = i2c_reg_icr[bus_num - 1];
+	reg_isr = i2c_reg_isr[bus_num - 1];
+	/* Is bus busy? */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_IBB)) {
+		pr_err("%s: bus is busy!\n", __func__);
+		return -1;
+	}
+	/* -----1. Send chip addr -------  */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write chip addr */
+	writel((addr << 1), reg_idbr);
+	/* Send start bit */
+	writel(readl(reg_icr) | ICR_START, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(reg_isr, ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_ACKNAK))
+		return -1;
+	/* -----2. Send reg addr ------ */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write reg addr */
+	writel(reg, reg_idbr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(reg_isr, ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_ACKNAK))
+		return -1;
+	/* -----3. Send val ------ */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write val */
+	writel(val, reg_idbr);
+	/* Send Stop bit */
+	writel(readl(reg_icr) | ICR_STOP, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(reg_isr, ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_ACKNAK))
+		return -1;
+
+	return 0;
+}
+
+int __raw_i2c_read_reg(u8 bus_num, u8 addr, u8 reg, u8 *buf, int len)
+{
+	u32 reg_idbr, reg_icr, reg_isr;
+	if (bus_num == 0 || bus_num > MAX_BUS_NUM) {
+		pr_err("%s: bus_num if out of range!\n", __func__);
+		return -1;
+	}
+	reg_idbr = i2c_reg_idbr[bus_num - 1];
+	reg_icr = i2c_reg_icr[bus_num - 1];
+	reg_isr = i2c_reg_isr[bus_num - 1];
+	/* Is bus busy? */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_IBB)) {
+		pr_err("%s: bus is busy!\n", __func__);
+		return -1;
+	}
+	/* -----1. Send chip addr -------  */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write chip addr */
+	writel((addr << 1), reg_idbr);
+	/* Send start bit */
+	writel(readl(reg_icr) | ICR_START, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(reg_isr, ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_ACKNAK))
+		return -1;
+	/* -----2. Send reg addr ------ */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write reg addr */
+	writel(reg, reg_idbr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(reg_isr, ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_ACKNAK))
+		return -1;
+	/* -----3. Start read sequence -------  */
+	/* Clear START and STOP bits */
+	writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+	writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+	/* Write chip addr: R/nW=1 */
+	writel((addr << 1) | 0x1, reg_idbr);
+	/* Send start bit */
+	writel(readl(reg_icr) | ICR_START, reg_icr);
+	/* Clear ALDIE */
+	writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+	/* Transfer, send a byte */
+	writel(readl(reg_icr) | ICR_TB, reg_icr);
+	/* Transmit empty? */
+	if (!i2c_isr_set_cleared(reg_isr, ISR_ITE, 0))
+		return -1;
+	/* Clear ITE: W1C */
+	writel(readl(reg_isr) | ISR_ITE, reg_isr);
+	/* Wait for ACK */
+	if (!i2c_isr_set_cleared(reg_isr, 0, ISR_ACKNAK))
+		return -1;
+	while (len--) {
+		/* Clear START and STOP bits */
+		writel(readl(reg_icr) & ~(ICR_START), reg_icr);
+		writel(readl(reg_icr) & ~(ICR_STOP), reg_icr);
+		if (len == 0)
+			/* Send NACK */
+			writel(readl(reg_icr) | ICR_ACKNAK, reg_icr);
+		else
+			/* Send ACK */
+			writel(readl(reg_icr) & ~ICR_ACKNAK, reg_icr);
+		/* Clear ALDIE */
+		writel(readl(reg_icr) & ~(ICR_ALDIE), reg_icr);
+		/* Transfer, receive a byte */
+		writel(readl(reg_icr) | ICR_TB, reg_icr);
+		/* Receive register full? */
+		if (!i2c_isr_set_cleared(reg_isr, ISR_IRF, 0))
+			return -1;
+		*buf = readl(reg_idbr);
+		buf++;
+		/* Clear ITE: W1C */
+		writel(readl(reg_isr) | ISR_IRF, reg_isr);
+	}
+	return 0;
+}
+#endif
+
 #define REG_RTC_BR0	(APB_VIRT_BASE + 0x010014)
 
 #define MPMU_APRR_WDTR	(1<<4)
