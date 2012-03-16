216790faa9fcf9eb5537bf05042836bbc50c1c5f
Mingliang Hu <mhu4@marvell.com>
2012-03-16 15:17:49 +0800

    mk2: add thermal device for mk2
    Change-Id: I5bd3ef0067b22a4eed43cce114ecab7b8fe2d793
    Signed-off-by: Mingliang Hu <mhu4@marvell.com>

--- arch/arm/mach-mmp/mk2.c
+++ arch/arm/mach-mmp/mk2.c
@@ -1385,6 +1385,7 @@ static void __init mk2_init(void)
 	mmp3_add_pwm(3);
 	platform_device_register(&mk2_lcd_backlight_devices);
 
+	mmp3_add_thermal();
 #ifdef CONFIG_ANDROID_PMEM
 	pxa_add_pmem();
 #endif
