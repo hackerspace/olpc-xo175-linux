16cef6a2809630e5f00419dc00b0a4775d67c0ad
Qing Xu <qingx@marvell.com>
2012-03-20 16:15:00 +0800

    ARM: pxa: uio hdmi add dvfm constrain for nevo
    Change-Id: I2b485d4bac588675ea6c86f7b0060880adeaf2d7
    Signed-off-by: Qing Xu <qingx@marvell.com>

--- drivers/uio/uio_hdmi.c
+++ drivers/uio/uio_hdmi.c
@@ -22,9 +22,12 @@
 #include <linux/pm_qos_params.h>
 #include <mach/uio_hdmi.h>
 #include <linux/earlysuspend.h>
-#ifndef CONFIG_CPU_PXA978
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 #include <mach/addr-map.h>
 #include <mach/cputype.h>
+#endif
+#ifdef CONFIG_CPU_PXA978
+#include <mach/dvfm.h>
 #endif
 #include <plat/pm.h>
 
@@ -33,6 +36,7 @@ static int early_suspend_flag;
 static int late_disable_flag;
 enum connect_lock con_lock;
 static bool timer_inited = 0;
+static int dvfm_dev_idx;
 
 struct hdmi_instance {
 	struct clk *clk;
@@ -51,23 +55,46 @@ struct hdmi_instance {
 	struct pm_qos_request_list qos_cpufreq_disable;
 	struct pm_qos_request_list qos_idle;
 };
-#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 
 static void set_power_constraint(struct hdmi_instance *hi, int min)
 {
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 	pm_qos_update_request(&hi->qos_idle, EXIT_LATENCY_CORE_EXTIDLE);
 	pm_qos_update_request(&hi->qos_cpufreq_min, min);
 	pm_qos_update_request(&hi->qos_cpufreq_disable, 1);
+#endif
+
+#ifdef CONFIG_CPU_PXA978
+	printk("hdmi: set_power_constraint\n");
+	/* Disable Lowpower mode */
+	dvfm_disable_op_name("156M", dvfm_dev_idx);
+	dvfm_disable_op_name("156M_HF", dvfm_dev_idx);
+	dvfm_disable_op_name("312M", dvfm_dev_idx);
+	dvfm_disable_op_name("416M", dvfm_dev_idx);
+	dvfm_disable_op_name("624M", dvfm_dev_idx);
+#endif
 }
 
 static void unset_power_constraint(struct hdmi_instance *hi)
 {
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 	pm_qos_update_request(&hi->qos_idle, PM_QOS_DEFAULT_VALUE);
 	pm_qos_update_request(&hi->qos_cpufreq_min, PM_QOS_DEFAULT_VALUE);
 	pm_qos_update_request(&hi->qos_cpufreq_disable, PM_QOS_DEFAULT_VALUE);
+#endif
+
+#ifdef CONFIG_CPU_PXA978
+	printk("hdmi: unset_power_constraint\n");
+	/* Enable Lowpower mode */
+	dvfm_enable_op_name("156M", dvfm_dev_idx);
+	dvfm_enable_op_name("156M_HF", dvfm_dev_idx);
+	dvfm_enable_op_name("312M", dvfm_dev_idx);
+	dvfm_enable_op_name("416M", dvfm_dev_idx);
+	dvfm_enable_op_name("624M", dvfm_dev_idx);
+#endif
 }
 
-
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 static u32 hdmi_direct_read(unsigned addr)
 {
        u32 hdmi_addr = AXI_VIRT_BASE + 0xbc00;
@@ -81,6 +108,7 @@ static void hdmi_direct_write(unsigned addr, unsigned data)
 	__raw_writel(data, hdmi_addr + addr);
 }
 #endif
+
 int hdmi_open(struct uio_info *info, struct inode *inode, void *file_priv)
 {
 	return 0;
@@ -150,10 +178,12 @@ static int hdmi_ioctl(struct uio_info *info, unsigned cmd, unsigned long arg,
 		break;
 #ifdef CONFIG_CPU_PXA978
 	case HDMI_PLL_ENABLE:
+		set_power_constraint(hi, -1);
 		clk_enable(hi->clk);
 		break;
 	case HDMI_PLL_DISABLE:
 		clk_disable(hi->clk);
+		unset_power_constraint(hi);
 		break;
 	case HDMI_PLL_SETRATE:
 		if (copy_from_user(&hdmi_freq, argp, sizeof(hdmi_freq)))
@@ -193,24 +223,19 @@ EXPORT_SYMBOL(hdmi_3d_sync_view);
 #ifdef CONFIG_HAS_EARLYSUSPEND
 static void hdmi_early_suspend(struct early_suspend *h)
 {
-#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 	struct hdmi_instance *hi =
 		container_of(h, struct hdmi_instance, early_suspend);
 	if (atomic_read(&hdmi_state) == 1)
 		unset_power_constraint(hi);
-#endif
 	early_suspend_flag = 1;
-
 	return;
 }
 static void hdmi_late_resume(struct early_suspend *h)
 {
-#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 	struct hdmi_instance *hi =
 		container_of(h, struct hdmi_instance, early_suspend);
 	if (atomic_read(&hdmi_state) == 1)
 		set_power_constraint(hi, HDMI_FREQ_CONSTRAINT);
-#endif
 	early_suspend_flag = 0;
 	return;
 }
@@ -258,10 +283,10 @@ static void hdmi_delayed_func(struct work_struct *work)
 	struct hdmi_instance *hi = container_of((struct delayed_work *)work,
 			struct hdmi_instance, hpd_work);
 	if (late_disable_flag) {
-#ifndef CONFIG_CPU_PXA978
+#if defined(CONFIG_CPU_MMP2) || defined(CONFIG_CPU_MMP3)
 		if (cpu_is_mmp2())
 #endif
-			clk_disable(hi->clk);
+		clk_disable(hi->clk);
 		late_disable_flag = 0;
 	} else {
 		/* send connect event to upper layer */
@@ -439,6 +464,7 @@ static int hdmi_probe(struct platform_device *pdev)
 	}
 
 #ifdef CONFIG_CPU_PXA978
+	dvfm_register("uio-hdmi", &dvfm_dev_idx);
 	if (hi->hdmi_power)
 		hi->hdmi_power(1); /* nevo need 5v to detect hpd*/
 #endif
