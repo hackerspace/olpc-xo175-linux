94344934035b71299db37e7b806e85b90357dff3
Sarah Zhang <xiazh@marvell.com>
2012-05-04 15:16:48 +0800

    camera: align code with nevo platform
    Ov9740 has already worked on nevo platform.
    We can share ov9740.c on orchid board.
    
    Change-Id: Id53254cbab1f4123ea98165f1d2fa5ea8726e40f
    Signed-off-by: Sarah Zhang <xiazh@marvell.com>

--- arch/arm/mach-mmp/include/mach/camera.h
+++ arch/arm/mach-mmp/include/mach/camera.h
@@ -25,10 +25,24 @@ struct mv_cam_pdata {
 struct clk;
 struct sensor_platform_data {
 	int id;
+	int interface;	/* MIPI or DVP flags*/
 	int (*power_on)(int, int);
 	int (*platform_set)(int, struct clk*);
 };
 
+struct mipi_phy {
+	u16 cl_termen;
+	u16 cl_settle;
+	u16 cl_miss;
+	u16 hs_termen;
+	u16 hs_settle;
+	u16 hs_rx_to;
+	u16 lane;	/* When set to 0, S/W will try to figure out a value */
+	u16 vc;		/* Virtual channel */
+	u16 dt1;	/* Data type 1: For video or main data type */
+	u16 dt2;	/* Data type 2: For thumbnail or auxiliry data type */
+};
+#define V4L2_CID_PRIVATE_GET_MIPI_PHY	(V4L2_CID_PRIVATE_BASE + 1)
 void mv_set_sensor_attached(bool sensor_attached);
 int isppwr_power_control(int on);
 #endif
