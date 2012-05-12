2f37c8eec988d7b3dc7c16291bb7dacbc46b37ee
Xiaofan Tian <tianxf@marvell.com>
2012-05-12 14:15:53 +0800

    ARM: pxa978: fix audio mfpr configuration
    Some audio related pins need to be configured
    dynamically when using different audio paths.
    
    But current code will modify all mfpr fields to
    default value. This patch will only modify function
    field.
    
    Change-Id: Ib6ea3e327e7107b2a1013cc5c8bd0e678afe17d3
    Signed-off-by: Xiaofan Tian <tianxf@marvell.com>

--- arch/arm/mach-pxa/saarc.c
+++ arch/arm/mach-pxa/saarc.c
@@ -2324,54 +2324,34 @@ static mfp_cfg_t pxa95x_hsl_mfp_cfg[] = {
 	MFP121_HSL_DATA4
 };
 
-static mfp_cfg_t pxa95x_abu_mfp_cfg[] = {
-	/* ABU of MG1 */
-	GPIO63_ABU_RXD,
-	GPIO64_ABU_TXD,
-	GPIO65_GPIO,	/* no use for ABU/SSI, and configure GPIO70 to AF0 to save power when using ABU (~0.5mA) */
-	GPIO66_ABU_FRM,
-	GPIO67_ABU_CLK,
-};
-
-static mfp_cfg_t pxa95x_bssp2_mfp_cfg[] = {
-	/* BSSP2 of MG1 */
-	GPIO63_SSP2_RXD,
-	GPIO64_SSP2_TXD,
-	GPIO65_SSP2_SYSCLK,
-	GPIO66_SSP2_FRM,
-	GPIO67_SSP2_CLK,
-};
-
-static mfp_cfg_t bssp3_mfp_cfg[] = {
-	/* BSSP3 of MG1*/
-	GPIO58_BSSP3_CLK,
-	GPIO59_BSSP3_FRM,
-	GPIO60_BSSP3_TXD,
-	GPIO61_BSSP3_RXD,
-};
-
-static mfp_cfg_t gssp1_mfp_cfg[] = {
-	/* BSSP3 of MG1*/
-	GPIO58_GSSP1_CLK,
-	GPIO59_GSSP1_FRM,
-	GPIO60_GSSP1_TXD,
-	GPIO61_GSSP1_RXD,
-};
-
 static void abu_mfp_init(bool abu)
 {
-	if (abu)
-		pxa3xx_mfp_config(ARRAY_AND_SIZE(pxa95x_abu_mfp_cfg));
-	else
-		pxa3xx_mfp_config(ARRAY_AND_SIZE(pxa95x_bssp2_mfp_cfg));
+	int i;
+	unsigned long mfpr, af;
+
+	af = abu ? 2 : 1;
+
+	for (i = 63; i < 68; i++) {
+		mfpr = mfp_read(i);
+		mfpr &= ~MFPR_AF_MASK;
+		mfpr |= MFPR_AF_SEL(af);
+		mfp_write(i, mfpr);
+	}
 }
 
 static void ssp3_mfp_init(bool bssp)
 {
-	if (bssp)
-		pxa3xx_mfp_config(ARRAY_AND_SIZE(bssp3_mfp_cfg));
-	else
-		pxa3xx_mfp_config(ARRAY_AND_SIZE(gssp1_mfp_cfg));
+	int i;
+	unsigned long mfpr, af;
+
+	af = bssp ? 1 : 2;
+
+	for (i = 58; i < 62; i++) {
+		mfpr = mfp_read(i);
+		mfpr &= ~MFPR_AF_MASK;
+		mfpr |= MFPR_AF_SEL(af);
+		mfp_write(i, mfpr);
+	}
 }
 
 #ifdef CONFIG_PM
