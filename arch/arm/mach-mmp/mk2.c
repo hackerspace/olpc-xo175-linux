da50e628d8dfec28d50a3a91430b4fa4f125a349
Yipeng Yao <ypyao@marvell.com>
2012-06-11 14:58:05 +0800

    ARM: MMP3: MK2: set 0.8V for vcc_main at suspend
    Change-Id: I940df0b189fb6eb4696ab117bb3409ed6cee35f6
    Signed-off-by: Yipeng Yao <ypyao@marvell.com>

--- arch/arm/mach-mmp/mk2.c
+++ arch/arm/mach-mmp/mk2.c
@@ -663,7 +663,7 @@ static int mk2_max77601_setup(struct max77601_chip *chip)
 			MAX77601_AME4_MASK, MAX77601_AME4_MASK);
 
 	/* Set suspend voltage(DVSSD0) */
-	data = 0x20; /* 1.0V */
+	data = 0x10; /* 0.8V */
 	max77601_write(chip, MAX77601_VREG_DVSSD0, &data, 1);
 	/* Set VCC_CORE(SD0) work at forced PWM mode */
 	max77601_set_bits(chip, MAX77601_VREG_SD0_CFG, (1 << 2), (1 << 2));
