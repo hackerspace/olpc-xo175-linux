a98e73d588b83d042c740471fb5e31cdd502414d
Wenzeng Chen <wzch@marvell.com>
2012-06-14 17:04:24 +0800

    isl9226:disable charger by manually
    1. add a delaywork to query battery capacity, when battery is full
       disable charging.  isl9226 has auto stop function, but most of
       the time we can't reach the EOC condition.
       for the EOC current = battery + system consumption
       such as EOC = 100mA, when system consumption > 100mA, charger
       never stop until timeout. so add delaywork to query battery status,
       when battery is full, disable charger, when battery less than
       threshold, re-enable charger
    2. add POWER_SUPPLY_PROP_STATUS to be used by max17043 driver. max17043
       can't detect current, add this property to get by battery driver to
       decide it is charging or not charging.
    3. refine the code.
    
    Change-Id: I85c25fba90b181d5ed0e53af8b9d8b507f76bb18
    Signed-off-by: Wenzeng Chen <wzch@marvell.com>

--- drivers/power/isl9226_charger.c
+++ drivers/power/isl9226_charger.c
@@ -27,8 +27,11 @@
 #include <linux/proc_fs.h>
 #include <plat/pm.h>
 #include <plat/usb.h>
-#include <linux/interrupt.h>
-#include <linux/gpio.h>
+
+#define ARRAY_AND_SIZE(x)	(x), ARRAY_SIZE(x)
+#define MAX_CAPACITY	100
+#define RECHARGE_CAPACITY	80
+#define UPDATA_INTERVAL		(30*HZ)
 
 struct isl9226_device_info {
 	struct device *dev;
@@ -36,107 +39,67 @@ struct isl9226_device_info {
 	struct power_supply ac;
 	struct power_supply usb;
 	struct notifier_block chg_notif;
+	struct delayed_work chg_update_work;
+	int interval;
+	int ischarging;
 	int ac_chg_online;
 	int usb_chg_online;
-	unsigned char chg_cur_in;
-	unsigned char chg_cur_out;
-	unsigned char prechg_cur;
-	unsigned char prechg_vol;
-	unsigned char eoc_cur;
-	unsigned char usb_chg_cur;
-	unsigned char default_chg_cur;
-	unsigned char ac_chg_cur;
+	u8 chg_cur_in;
+	u8 chg_cur_out;
+	u8 prechg_cur;
+	u8 prechg_vol;
+	u8 eoc_cur;
+	u8 usb_chg_cur;
+	u8 default_chg_cur;
+	u8 ac_chg_cur;
 };
-static const unsigned int input_current_table[] = {
+static const u32 input_current_table[] = {
 	95, 475, 855, 950, 1425, 2900, 2900, 2900,
 };
-static const unsigned int eoc_current_table[] = {
+
+static const u32 eoc_current_table[] = {
 	50, 75, 100,
 };
-static const unsigned int prechg_current_table[] = {
-	130, 150, 180,
+
+static const u32 prechg_current_table[] = {
+	120, 150, 180,
 };
-static const unsigned int prechg_voltage_table[] = {
+
+static const u32 prechg_voltage_table[] = {
 	3000, 2900, 2800, 2700,
 };
+
 static char *isl9226_supply_to[] = {
 	"battery",
 };
 
 static enum power_supply_property isl9226_ac_props[] = {
+	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_ONLINE,
 };
 
 static enum power_supply_property isl9226_usb_props[] = {
+	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_ONLINE,
 };
 
-static int isl9226_get_input_current(unsigned int cur)
+static int isl9226_get_table_data(const u32 *table, u32 size, u32 data)
 {
-	int i, ret = -ENOENT;
-	for (i = 0; i < sizeof(input_current_table); i++) {
-		if (cur == input_current_table[i]) {
+	int i, ret = -1;
+	for (i = 0; i < size; i++) {
+		if (data == table[i]) {
 			ret = i;
 			break;
 		}
 	}
 	if (ret < 0) {
 		ret = 0;
-		pr_err("invalid precharge current %d mA\n", cur);
+		pr_err("invalid input data %d\n", data);
 	}
 	return ret;
 }
 
-static int isl9226_get_eoc_current(unsigned int cur)
-{
-	int i, ret = -ENOENT;
-	for (i = 0; i < sizeof(eoc_current_table); i++) {
-		if (cur == eoc_current_table[i]) {
-			ret = i;
-			break;
-		}
-	}
-	if (ret < 0) {
-		ret = 0;
-		pr_err("invalid end of charge current %d mA\n", cur);
-	}
-	return ret;
-}
-
-static int isl9226_get_prechg_current(unsigned int cur)
-{
-	int i, ret = -ENOENT;
-	for (i = 0; i < sizeof(prechg_current_table); i++) {
-		if (cur == prechg_current_table[i]) {
-			ret = i;
-			break;
-		}
-	}
-	if (ret < 0) {
-		ret = 0;
-		pr_err("invalid precharge current %d mA\n", cur);
-	}
-	return ret;
-}
-
-static int isl9226_get_prechg_voltage(unsigned int vol)
-{
-	int i, ret = -ENOENT;
-	for (i = 0; i < sizeof(prechg_voltage_table); i++) {
-		if (vol == prechg_voltage_table[i]) {
-			ret = i;
-			break;
-		}
-	}
-	if (ret < 0) {
-		ret = 0;
-		pr_err("invalid precharge voltage %d mV\n", vol);
-	}
-	return ret;
-}
-
-static int isl9226_write_reg(struct i2c_client *client, unsigned char reg,
-			     unsigned char data)
+static int isl9226_write_reg(struct i2c_client *client, u8 reg, u8 data)
 {
 	int ret = 0;
 	if (!client)
@@ -150,10 +113,10 @@ static int isl9226_write_reg(struct i2c_client *client, unsigned char reg,
 	return 0;
 }
 
-static int isl9226_read_reg(struct i2c_client *client, unsigned char reg)
+static int isl9226_read_reg(struct i2c_client *client, u8 reg, u8 * data)
 {
 	int ret = 0;
-	if (!client)
+	if (!client || !data)
 		return -EINVAL;
 	ret = i2c_smbus_read_byte_data(client, reg);
 	if (ret < 0) {
@@ -161,22 +124,242 @@ static int isl9226_read_reg(struct i2c_client *client, unsigned char reg)
 			reg);
 		return ret;
 	}
-	return (unsigned char)ret;
+	*data = ret;
+	return 0;
 }
 
-static int isl9226_set_bits(struct i2c_client *client, unsigned char reg,
-			    unsigned char mask, unsigned char data)
+static int isl9226_set_charge(struct isl9226_device_info *info, bool enable)
 {
-	unsigned char valget, valset;
 	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return ret;
+	if (enable)
+		value |= CHGEN;
+	else
+		value &= ~CHGEN;
+	return isl9226_write_reg(info->client, OPMOD, value);
+}
 
-	/*need to add mutex protection */
-	valget = isl9226_read_reg(client, reg);
-	valset = valget;
-	valset &= ~mask;
-	valset |= data;
-	ret = isl9226_write_reg(client, reg, valset);
-	return ret;
+static int isl9226_set_autostop(struct isl9226_device_info *info, bool enable)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return ret;
+	if (enable)
+		value |= AUTOSTP;
+	else
+		value &= ~AUTOSTP;
+	return isl9226_write_reg(info->client, OPMOD, value);
+}
+
+static int isl9226_set_autorestart(struct isl9226_device_info *info,
+				   bool enable)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return ret;
+	if (enable)
+		value |= AUTORES;
+	else
+		value &= ~AUTORES;
+	return isl9226_write_reg(info->client, OPMOD, value);
+}
+
+static int isl9226_set_eocsppm(struct isl9226_device_info *info, bool enable)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return ret;
+	if (enable)
+		value |= EOCSPPM;
+	else
+		value &= ~EOCSPPM;
+	return isl9226_write_reg(info->client, OPMOD, value);
+}
+
+static int isl9226_set_jeita(struct isl9226_device_info *info, bool enable)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return ret;
+	if (enable)
+		value |= JEITAEN;
+	else
+		value &= ~JEITAEN;
+	return isl9226_write_reg(info->client, OPMOD, value);
+}
+
+static int isl9226_set_enidpm(struct isl9226_device_info *info, bool enable)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return ret;
+	if (enable)
+		value |= ENIDPM;
+	else
+		value &= ~ENIDPM;
+	return isl9226_write_reg(info->client, OPMOD, value);
+}
+
+static int isl9226_set_eoc_cur(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, CCTRL1, &value);
+	if (ret)
+		return ret;
+	value = (value & 0x3F) | ((data & 0x03) << 6);
+	return isl9226_write_reg(info->client, CCTRL1, value);
+}
+
+static int isl9226_set_prechg_cur(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, CCTRL1, &value);
+	if (ret)
+		return ret;
+	value = (value & 0xCF) | ((data & 0x03) << 4);
+	return isl9226_write_reg(info->client, CCTRL1, value);
+}
+
+static int isl9226_set_current_limit(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, CCTRL1, &value);
+	if (ret)
+		return ret;
+	value = (value & 0xF8) | (data & 0x07);
+	return isl9226_write_reg(info->client, CCTRL1, value);
+}
+
+static int isl9226_set_charge_cur(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, CCTRL2, &value);
+	if (ret)
+		return ret;
+	value = (value & 0xF8) | (data & 0x07);
+	return isl9226_write_reg(info->client, CCTRL2, value);
+}
+
+static int isl9226_set_prechg_vol(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, VCTRL1, &value);
+	if (ret)
+		return ret;
+	value = (value & 0x3F) | ((data & 0x03) << 6);
+	return isl9226_write_reg(info->client, VCTRL1, value);
+}
+
+static int isl9226_set_vmax(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, VCTRL1, &value);
+	if (ret)
+		return ret;
+	value = (value & 0xE0) | (data & 0x1F);
+	return isl9226_write_reg(info->client, VCTRL1, value);
+}
+
+static int isl9226_set_vsppm(struct isl9226_device_info *info, u8 data)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, VCTRL2, &value);
+	if (ret)
+		return ret;
+	value = (value & 0x3F) | ((data & 0x03) << 6);
+	return isl9226_write_reg(info->client, VCTRL2, value);
+}
+
+static int isl9226_is_charge_enabled(struct isl9226_device_info *info)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, OPMOD, &value);
+	if (ret)
+		return 0;
+	else
+		return value & CHGEN;
+}
+
+static int isl9226_get_sta1(struct isl9226_device_info *info)
+{
+	int ret;
+	u8 value;
+	ret = isl9226_read_reg(info->client, STAT1, &value);
+	if (ret)
+		return 0;
+	else
+		return value;
+}
+
+static int isl9226_check_battery_full(struct isl9226_device_info *info)
+{
+	struct power_supply *psy;
+	union power_supply_propval val;
+	int i, ret = 0;
+
+	for (i = 0; i < info->ac.num_supplicants; i++) {
+		psy = power_supply_get_by_name(info->ac.supplied_to[i]);
+		if (!psy || !psy->get_property)
+			continue;
+		ret = psy->get_property(psy, POWER_SUPPLY_PROP_STATUS, &val);
+		if (ret == 0 && val.intval == POWER_SUPPLY_STATUS_FULL)
+			return 1;
+	}
+	for (i = 0; i < info->usb.num_supplicants; i++) {
+		psy = power_supply_get_by_name(info->usb.supplied_to[i]);
+		if (!psy || !psy->get_property)
+			continue;
+		ret = psy->get_property(psy, POWER_SUPPLY_PROP_STATUS, &val);
+		if (ret == 0 && val.intval == POWER_SUPPLY_STATUS_FULL)
+			return 1;
+	}
+	return 0;
+}
+
+static int isl9226_get_battery_capacity(struct isl9226_device_info *info)
+{
+	struct power_supply *psy;
+	union power_supply_propval val;
+	int i, ret = 0;
+
+	for (i = 0; i < info->ac.num_supplicants; i++) {
+		psy = power_supply_get_by_name(info->ac.supplied_to[i]);
+		if (!psy || !psy->get_property)
+			continue;
+		ret = psy->get_property(psy, POWER_SUPPLY_PROP_CAPACITY, &val);
+		if (ret == 0)
+			return val.intval;
+	}
+	for (i = 0; i < info->usb.num_supplicants; i++) {
+		psy = power_supply_get_by_name(info->usb.supplied_to[i]);
+		if (!psy || !psy->get_property)
+			continue;
+		ret = psy->get_property(psy, POWER_SUPPLY_PROP_CAPACITY, &val);
+		if (ret == 0)
+			return val.intval;
+	}
+	return 0;
 }
 
 static int isl9226_has_supplicant(struct isl9226_device_info *info)
@@ -209,31 +392,79 @@ static int isl9226_has_supplicant(struct isl9226_device_info *info)
 */
 static int isl9226_start_charging(struct isl9226_device_info *info)
 {
-	unsigned char value;
-	if (!isl9226_has_supplicant(info))
+	if (!isl9226_has_supplicant(info)) {
+		dev_err(info->dev, "haven't supplicant, but start charging\n");
 		return 0;
-	value =
-	    ((info->eoc_cur & 0x3) << 6) | ((info->prechg_cur & 0x03) << 4) |
-	    (info->chg_cur_out & 0x07);
+	}
 
-	/*set EOC, precharge and output current */
-	isl9226_write_reg(info->client, CCTRL1, value);
+	if (!info->usb_chg_online && !info->ac_chg_online) {
+		dev_err(info->dev, "haven't charger, but start charging\n");
+		return 0;
+	}
+	/*enable auto stop */
+	isl9226_set_autostop(info, 1);
 
-	/*set input current */
-	isl9226_write_reg(info->client, CCTRL2, info->chg_cur_in & 0x07);
+	/*enable auto restart */
+	isl9226_set_autorestart(info, 1);
 
-	/*set precharge voltage, charge voltage 4.2v */
-	isl9226_write_reg(info->client, VCTRL1, ((info->prechg_vol & 0x03) << 6) | 0x13);
+	/*disable eocsppm, use charger as power supply when eoc */
+	isl9226_set_eocsppm(info, 0);
 
-	/*enable charge, auto-recharge, auto-stop */
-	isl9226_write_reg(info->client, OPMOD, 0x27);
-	return 0;
+	/*disable jeita, we don't use that */
+	isl9226_set_jeita(info, 0);
+
+	/*set EOC detection current */
+	isl9226_set_eoc_cur(info, info->eoc_cur);
+
+	/*set precharge current */
+	isl9226_set_prechg_cur(info, info->prechg_cur);
+
+	/*set out put current limit */
+	isl9226_set_current_limit(info, info->chg_cur_out);
+
+	/*set input current limit */
+	isl9226_set_charge_cur(info, info->chg_cur_in);
+
+	/*set precharge voltage */
+	isl9226_set_prechg_vol(info, info->prechg_vol);
+
+	/*set vmax to 4.2V */
+	isl9226_set_vmax(info, VMAX4_2V);
+
+	/*enable charger */
+	isl9226_set_charge(info, 1);
+
+	info->ischarging = 1;
+	return 1;
 }
 
 static void isl9226_stop_charging(struct isl9226_device_info *info)
 {
 	if (info->usb_chg_online || info->ac_chg_online)
-		isl9226_write_reg(info->client, OPMOD, 0);
+		isl9226_set_charge(info, 0);
+	info->ischarging = 0;
+}
+
+static void isl9226_update_work_func(struct work_struct *work)
+{
+	struct isl9226_device_info *info =
+				container_of(work, struct isl9226_device_info,
+						chg_update_work.work);
+	int capacity;
+	if (isl9226_check_battery_full(info)) {
+		if (info->ischarging) {
+			isl9226_stop_charging(info);
+			dev_info(info->dev, "battery full, disable charge\n");
+		}
+	}
+	capacity = isl9226_get_battery_capacity(info);
+	if (capacity <= RECHARGE_CAPACITY) {
+		if (!info->ischarging) {
+			isl9226_start_charging(info);
+			dev_info(info->dev, "re-enable charge\n");
+		}
+	}
+	schedule_delayed_work(&info->chg_update_work, info->interval);
 }
 
 static int isl9226_chg_notifier_callback(struct notifier_block *nb,
@@ -274,10 +505,13 @@ static int isl9226_chg_notifier_callback(struct notifier_block *nb,
 		break;
 	}
 	if (info->usb_chg_online || info->ac_chg_online) {
-		isl9226_start_charging(info);
+		if (isl9226_start_charging(info))
+			schedule_delayed_work(&info->chg_update_work,
+					      info->interval);
 	} else {
 		/*cable plug out, isl9226 power down, i2c read write error */
 		isl9226_stop_charging(info);
+		cancel_delayed_work_sync(&info->chg_update_work);
 	}
 	power_supply_changed(&info->ac);
 	power_supply_changed(&info->usb);
@@ -294,6 +528,12 @@ static int isl9226_ac_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_ONLINE:
 		val->intval = info->ac_chg_online;
 		break;
+	case POWER_SUPPLY_PROP_STATUS:
+		if (info->ischarging)
+			val->intval = POWER_SUPPLY_STATUS_CHARGING;
+		else
+			val->intval = POWER_SUPPLY_STATUS_NOT_CHARGING;
+		break;
 	default:
 		return -EINVAL;
 	}
@@ -309,6 +549,12 @@ static int isl9226_usb_get_property(struct power_supply *psy,
 	switch (psp) {
 	case POWER_SUPPLY_PROP_ONLINE:
 		val->intval = info->usb_chg_online;
+		break;
+	case POWER_SUPPLY_PROP_STATUS:
+		if (info->ischarging)
+			val->intval = POWER_SUPPLY_STATUS_CHARGING;
+		else
+			val->intval = POWER_SUPPLY_STATUS_NOT_CHARGING;
 		break;
 	default:
 		return -EINVAL;
@@ -335,13 +581,25 @@ static int isl9226_charger_probe(struct i2c_client *client,
 	info->client = client;
 	info->dev = &client->dev;
 	i2c_set_clientdata(client, info);
-	info->eoc_cur = isl9226_get_eoc_current(pdata->eoc_current);
-	info->prechg_cur = isl9226_get_prechg_current(pdata->prechg_current);
-	info->prechg_vol = isl9226_get_prechg_voltage(pdata->prechg_voltage);
-	info->usb_chg_cur = isl9226_get_input_current(pdata->usb_input_current);
-	info->ac_chg_cur = isl9226_get_input_current(pdata->ac_input_current);
+	info->interval = UPDATA_INTERVAL;
+	info->eoc_cur =
+	    isl9226_get_table_data(ARRAY_AND_SIZE(eoc_current_table),
+				   pdata->eoc_current);
+	info->prechg_cur =
+	    isl9226_get_table_data(ARRAY_AND_SIZE(prechg_current_table),
+				   pdata->prechg_current);
+	info->prechg_vol =
+	    isl9226_get_table_data(ARRAY_AND_SIZE(prechg_voltage_table),
+				   pdata->prechg_voltage);
+	info->usb_chg_cur =
+	    isl9226_get_table_data(ARRAY_AND_SIZE(input_current_table),
+				   pdata->usb_input_current);
+	info->ac_chg_cur =
+	    isl9226_get_table_data(ARRAY_AND_SIZE(input_current_table),
+				   pdata->ac_input_current);
 	info->default_chg_cur =
-	    isl9226_get_input_current(pdata->default_input_current);
+	    isl9226_get_table_data(ARRAY_AND_SIZE(input_current_table),
+				   pdata->default_input_current);
 	info->ac.name = "ac";
 	info->ac.type = POWER_SUPPLY_TYPE_MAINS;
 	info->ac.supplied_to = isl9226_supply_to;
@@ -365,10 +623,10 @@ static int isl9226_charger_probe(struct i2c_client *client,
 	info->usb.num_properties = ARRAY_SIZE(isl9226_usb_props);
 	ret = power_supply_register(info->dev, &info->usb);
 	if (ret) {
-		dev_err(&client->dev,
-			"USB power supply resisteration failed! \n");
+		dev_err(&client->dev, "USB power supply resisteration failed! \n");
 		goto out;
 	}
+	INIT_DELAYED_WORK(&info->chg_update_work, isl9226_update_work_func);
 
 	info->chg_notif.notifier_call = isl9226_chg_notifier_callback;
 #ifdef CONFIG_USB_GADGET_PXA_U2O
@@ -391,6 +649,7 @@ static int isl9226_charger_remove(struct i2c_client *client)
 	struct isl9226_device_info *info = i2c_get_clientdata(client);
 	isl9226_stop_charging(info);
 	mv_udc_unregister_client(&info->chg_notif);
+	cancel_delayed_work_sync(&info->chg_update_work);
 	power_supply_unregister(&info->usb);
 	power_supply_unregister(&info->ac);
 	kfree(info);
--- include/linux/power/isl9226.h
+++ include/linux/power/isl9226.h
@@ -16,6 +16,15 @@
 #define JEITAEN		(1 << 4)
 #define ENIDPM		(1 << 5)
 #define SUSTIME	(1 << 6)
+
+#define CHGING		(1 << 0)
+#define BATFUL		(1 << 1)
+#define CONST_VOL	(1 << 2)
+#define EOC1		(1 << 3)
+#define PGOOD		(1 << 4)
+
+#define VMAX4_2V	(0x13)
+
 struct isl9226_charger_pdata {
 	unsigned int default_input_current;
 	unsigned int usb_input_current;
