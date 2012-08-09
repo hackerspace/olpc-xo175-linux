1e0ce5e19b914914f5630f783290fbf16aa2cd0e
Mingliang Hu <mhu4@marvell.com>
2012-08-09 18:22:43 +0800

    Merge linux-stable 3.0.28 into android-3.0
    Fix conflict in fs/proc/base.c
    
    Change-Id: I5e6ff8817bce6a797d041f85148b593a0e77f943
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    Signed-off-by: Mingliang Hu <mhu4@marvell.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -211,7 +211,8 @@ static struct mm_struct *mm_access(struct task_struct *task, unsigned int mode)
 
 	mm = get_task_mm(task);
 	if (mm && mm != current->mm &&
-			!ptrace_may_access(task, mode)) {
+			!ptrace_may_access(task, mode) &&
+			!capable(CAP_SYS_RESOURCE)) {
 		mmput(mm);
 		mm = ERR_PTR(-EACCES);
 	}
@@ -799,6 +800,8 @@ static ssize_t mem_rw(struct file *file, char __user *buf,
 			size_t count, loff_t *ppos, int write)
 {
 	struct mm_struct *mm = file->private_data;
+	unsigned long addr = *ppos;
+	ssize_t copied;
 	char *page;
 
 	if (!mm)
@@ -844,7 +847,6 @@ free:
 	free_page((unsigned long) page);
 	return copied;
 }
-#endif
 
 static ssize_t mem_read(struct file *file, char __user *buf,
 			size_t count, loff_t *ppos)
@@ -852,11 +854,16 @@ static ssize_t mem_read(struct file *file, char __user *buf,
 	return mem_rw(file, buf, count, ppos, 0);
 }
 
+#define mem_write NULL
+
+#ifndef mem_write
+/* This is a security hazard */
 static ssize_t mem_write(struct file *file, const char __user *buf,
 			 size_t count, loff_t *ppos)
 {
 	return mem_rw(file, (char __user*)buf, count, ppos, 1);
 }
+#endif
 
 loff_t mem_lseek(struct file *file, loff_t offset, int orig)
 {
