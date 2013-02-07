c606386682c2b00b458aee392f12ac6bbda5716d
Wen-chien Jesse Sung <jesse.sung@canonical.com>
2013-02-07 16:00:06 +0800

    Debianized
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

---
+++ debian.dajia/rules.d/armhf.mk
@@ -0,0 +1,19 @@
+build_arch	= arm
+header_arch	= arm
+asm_link	= arm
+defconfig	= defconfig
+flavours	= dajia
+build_image	= zImage
+kernel_file	= arch/$(build_arch)/boot/zImage
+install_file	= vmlinuz
+no_dumpfile	= true
+
+loader		= grub
+
+do_doc_package		= false
+do_source_package	= false
+do_common_headers_indep	= false
+do_libc_dev_package	= false
+disable_d_i		= true
+skipmodule		= true
+do_tools		= false
