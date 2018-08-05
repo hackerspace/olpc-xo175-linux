16ee3759970ab7347981ea7b30218561d31c6ac5
Lubomir Rintel <lkundrak@v3.sk>
2018-08-05 22:29:37 +0200

    x buildme config

---
+++ merge.sh
@@ -0,0 +1,21 @@
+set -x
+set -e
+
+BRANCHES=""
+#BRANCHES="$BRANCHES arm-soc/for-next"
+BRANCHES="$BRANCHES xo/lr/olpc-xo175-drm-dt-v5"
+BRANCHES="$BRANCHES xo/lr/olpc-xo175-fixes5-drm"
+BRANCHES="$BRANCHES xo/lr/olpc-xo175-fixes5-armada-drm"
+BRANCHES="$BRANCHES xo/lr/olpc-xo175-fixes10"
+#BRANCHES="$BRANCHES xo/lr/olpc-xo175-galcore-v1"
+BRANCHES="$BRANCHES xo/lr/mmp3-hsic-v2"
+
+git branch -D merged || :
+git checkout -b merged v5.5-rc4
+
+for B in $BRANCHES; do
+	EDITOR=: git merge --no-ff $B
+done
+
+git checkout lr/ariel
+git rebase merged
