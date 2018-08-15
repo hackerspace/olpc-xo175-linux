196d6fb67f982bc66bd74d8636d6462aba6f532b
Lubomir Rintel <lkundrak@v3.sk>
2018-08-16 00:26:25 +0200

    WIP spi: Deal with slaves that return from transfer_one() unfinished
    Some drivers, such as spi-pxa2xx return from the transfer_one callback
    immediately, idicating that the transfer will be finished asynchronously.
    
    Normally, spi_transfer_one_message() synchronously waits for the
    transfer to finish with wait_for_completion_timeout(). For slaves, we
    don't want the transaction to time out as it can complete in a long time
    in future. Use wait_for_completion_interruptible() instead.
    
    TODO: Restructure things to deal with too deep nesting
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1037,26 +1037,34 @@ static int spi_transfer_one_message(struct spi_controller *ctlr,
 
 			if (ret > 0) {
 				ret = 0;
-				ms = 8LL * 1000LL * xfer->len;
-				do_div(ms, xfer->speed_hz);
-				ms += ms + 200; /* some tolerance */
+				if (spi_controller_is_slave(ctlr)) {
+					if (wait_for_completion_interruptible(&ctlr->xfer_completion)) {
+						dev_dbg(&msg->spi->dev, "SPI transfer interrupted\n");
+						return -EINTR;
+					}
+				} else {
+					ms = 8LL * 1000LL * xfer->len;
+					do_div(ms, xfer->speed_hz);
+					ms += ms + 200; /* some tolerance */
 
-				if (ms > UINT_MAX)
-					ms = UINT_MAX;
+					if (ms > UINT_MAX)
+						ms = UINT_MAX;
 
-				ms = wait_for_completion_timeout(&ctlr->xfer_completion,
-								 msecs_to_jiffies(ms));
+					ms = wait_for_completion_timeout(&ctlr->xfer_completion,
+									 msecs_to_jiffies(ms));
+
+					if (ms == 0) {
+						SPI_STATISTICS_INCREMENT_FIELD(statm,
+									       timedout);
+						SPI_STATISTICS_INCREMENT_FIELD(stats,
+									       timedout);
+						dev_err(&msg->spi->dev,
+							"SPI transfer timed out\n");
+						msg->status = -ETIMEDOUT;
+					}
+				}
 			}
 
-			if (ms == 0) {
-				SPI_STATISTICS_INCREMENT_FIELD(statm,
-							       timedout);
-				SPI_STATISTICS_INCREMENT_FIELD(stats,
-							       timedout);
-				dev_err(&msg->spi->dev,
-					"SPI transfer timed out\n");
-				msg->status = -ETIMEDOUT;
-			}
 		} else {
 			if (xfer->len)
 				dev_err(&msg->spi->dev,
