d3bd2821be65d823d32cdf183bc8c30b26a0c45c
Lubomir Rintel <lkundrak@v3.sk>
2018-09-08 00:36:33 +0200

    Input: olpc_apsp: allocate the GPIOs used
    Take the GPIO lines are used by the SP. The driver doesn't touch the
    lines -- this is done to disallow anything else from fiddling with
    them because that would confuse the SP firmware.
    
    Also, the lines are now nicely visible in /sys/kernel/debug/gpio.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

--- drivers/input/serio/olpc_apsp.c
+++ drivers/input/serio/olpc_apsp.c
@@ -24,6 +24,7 @@
 #include <linux/slab.h>
 #include <linux/delay.h>
 #include <linux/clk.h>
+#include <linux/gpio/consumer.h>
 
 /*
  * The OLPC XO-1.75 and XO-4 laptops do not have a hardware PS/2 controller.
@@ -76,6 +77,8 @@ struct olpc_apsp {
 	struct serio *padio;
 	void __iomem *base;
 	struct clk *clk;
+	struct gpio_desc *clk_gpio;
+	struct gpio_desc *data_gpio;
 	int open_count;
 	int irq;
 };
@@ -210,6 +213,16 @@ static int olpc_apsp_probe(struct platform_device *pdev)
 	priv->clk = devm_clk_get(&pdev->dev, "sp");
 	if (IS_ERR(priv->clk))
 		return PTR_ERR(priv->clk);
+
+	priv->clk_gpio = devm_gpiod_get_optional(&pdev->dev, "clk",
+							GPIOD_ASIS);
+	if (IS_ERR(priv->clk_gpio))
+		return PTR_ERR(priv->clk_gpio);
+
+	priv->data_gpio = devm_gpiod_get_optional(&pdev->dev, "data",
+							GPIOD_ASIS);
+	if (IS_ERR(priv->data_gpio))
+		return PTR_ERR(priv->data_gpio);
 
 	/* KEYBOARD */
 	kb_serio = kzalloc(sizeof(struct serio), GFP_KERNEL);
