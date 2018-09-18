81c16fad3b993fb630fe32aae97ab68777049e20
Lubomir Rintel <lkundrak@v3.sk>
2018-09-18 22:34:05 +0200

    big logo

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -99,6 +99,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* SuperH Linux logo */
 		logo = &logo_superh_clut224;
 #endif
+#ifdef CONFIG_LOGO_BIG_CLUT224
+		/* Big Linux logo */
+		logo = &logo_big_clut224;
+#endif
 	}
 	return logo;
 }
