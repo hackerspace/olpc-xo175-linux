c3a924464e5eaa120c88d935010a9deb9db48c7e
Lubomir Rintel <lkundrak@v3.sk>
2019-01-30 20:14:39 +0100

    m

--- merge.sh
+++ merge.sh
@@ -1,19 +1,10 @@
 set -x
 set -e
 
-BRANCHES=""
-BRANCHES="$BRANCHES lr/olpc-xo175-fixes6"
-BRANCHES="$BRANCHES lr/olpc-xo175-fixes7-ec"
-BRANCHES="$BRANCHES lr/olpc-xo175-fixes7-battery"
-BRANCHES="$BRANCHES lr/olpc-xo175-fixes4-mmp-camera"
-
-
 git branch -D merged || :
-git checkout -b merged v5.0-rc4
+git checkout -b merged lr/olpc-xo175
 
-for B in $BRANCHES; do
-	EDITOR=: git merge --no-ff $B
-done
+EDITOR=: git pull git://git.armlinux.org.uk/~rmk/linux-arm drm-armada-devel
 
-git checkout lr/olpc-xo175
+git checkout lr/olpc-xo175-fixes3-drm
 git rebase merged
