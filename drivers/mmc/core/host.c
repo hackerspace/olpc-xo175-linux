ce16885d6917abbd53410667d408693adcaf7ce7
Lubomir Rintel <lkundrak@v3.sk>
2019-04-15 16:57:44 +0200

    mmc: core: try to use an id from the devicetree
    If there's a mmc* alias in the device tree, take the device number from
    it, so that we end up with a device name that matches the alias.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -408,7 +408,12 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 	/* scanning will be enabled when we're ready */
 	host->rescan_disable = 1;
 
-	err = ida_simple_get(&mmc_host_ida, 0, 0, GFP_KERNEL);
+	/* prefer an alias */
+	err = of_alias_get_id(dev->of_node, "mmc");
+	if (err < 0)
+		err = 0;
+
+	err = ida_simple_get(&mmc_host_ida, err, 0, GFP_KERNEL);
 	if (err < 0) {
 		kfree(host);
 		return NULL;
