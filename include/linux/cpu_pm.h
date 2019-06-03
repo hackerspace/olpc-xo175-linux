e39071ddc6616c8548dd44c38cf191ab7c4dfa9f
Lubomir Rintel <lkundrak@v3.sk>
2019-06-03 13:17:24 +0200

    CPU_PM

--- include/linux/cpu_pm.h
+++ include/linux/cpu_pm.h
@@ -66,4 +66,44 @@ enum cpu_pm_event {
 	CPU_CLUSTER_PM_EXIT,
 };
 
+#ifdef CONFIG_CPU_PM
+int cpu_pm_register_notifier(struct notifier_block *nb);
+int cpu_pm_unregister_notifier(struct notifier_block *nb);
+int cpu_pm_enter(void);
+int cpu_pm_exit(void);
+int cpu_cluster_pm_enter(void);
+int cpu_cluster_pm_exit(void);
+
+#else
+
+static inline int cpu_pm_register_notifier(struct notifier_block *nb)
+{
+	return 0;
+}
+
+static inline int cpu_pm_unregister_notifier(struct notifier_block *nb)
+{
+	return 0;
+}
+
+static inline int cpu_pm_enter(void)
+{
+	return 0;
+}
+
+static inline int cpu_pm_exit(void)
+{
+	return 0;
+}
+
+static inline int cpu_cluster_pm_enter(void)
+{
+	return 0;
+}
+
+static inline int cpu_cluster_pm_exit(void)
+{
+	return 0;
+}
+#endif
 #endif
