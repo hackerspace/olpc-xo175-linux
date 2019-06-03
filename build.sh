0357e3d6008ca6504506efdd9775da61485f6a68
Lubomir Rintel <lkundrak@v3.sk>
2019-06-03 16:34:54 +0200

    b

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ set -x
 set -e
 
 # curl -L https://releases.linaro.org/components/toolchain/binaries/4.9-2016.02/arm-linux-gnueabihf/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf.tar.xz |tar xJf - -C /opt
-CROSS_COMPILE=/opt/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- ARCH=arm make mmp3_ariel_defconfig
+#CROSS_COMPILE=/opt/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- ARCH=arm make mmp3_ariel_defconfig
 CROSS_COMPILE=/opt/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- ARCH=arm make -j$(nproc) -k uImage
 #CROSS_COMPILE=/opt/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- ARCH=arm make -k uImage V=1
