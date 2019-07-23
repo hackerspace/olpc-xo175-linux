55445c2c17b3243fa8c3f04e6fa995f4f1b7b36d
Lubomir Rintel <lkundrak@v3.sk>
2019-07-23 11:31:40 +0200

    commit fcd70cd36b9bf697122538c9e38e8cf954b2342b Author: Daniel Vetter <daniel.vetter@ffwll.ch> Date:   Thu Jan 17 22:03:34 2019 +0100
        drm: Split out drm_probe_helper.h

--- drivers/gpu/drm/bridge/chrontel-ch7033.c
+++ drivers/gpu/drm/bridge/chrontel-ch7033.c
@@ -5,7 +5,7 @@
 #include <drm/drmP.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc.h>
-#include <drm/drm_crtc_helper.h>
+#include <drm/drm_probe_helper.h>
 
 #define CH7033_PAGE_SEL_REG		0x03
 
