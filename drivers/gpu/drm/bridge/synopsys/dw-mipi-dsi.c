3cd4008845f3dbbd8fc2570fcd85b66d778276b3
Mihail Atanassov <Mihail.Atanassov@arm.com>
2019-11-26 13:16:15 +0000

    drm/bridge/synopsys: dsi: Use drm_bridge_init()
    No functional change.
    
    Signed-off-by: Mihail Atanassov <mihail.atanassov@arm.com>

--- drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
@@ -1067,11 +1067,8 @@ __dw_mipi_dsi_probe(struct platform_device *pdev,
 		return ERR_PTR(ret);
 	}
 
-	dsi->bridge.driver_private = dsi;
-	dsi->bridge.funcs = &dw_mipi_dsi_bridge_funcs;
-#ifdef CONFIG_OF
-	dsi->bridge.of_node = pdev->dev.of_node;
-#endif
+	drm_bridge_init(&dsi->bridge, &pdev->dev, &dw_mipi_dsi_bridge_funcs,
+			NULL, dsi);
 
 	return dsi;
 }
