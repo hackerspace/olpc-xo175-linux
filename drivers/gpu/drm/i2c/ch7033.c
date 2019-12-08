7b2dcceb873b4247f5f917f75c884ebf2f79f3d9
Lubomir Rintel <lkundrak@v3.sk>
2019-12-08 18:55:26 +0100

    ch7033edid

--- drivers/gpu/drm/i2c/ch7033.c
+++ drivers/gpu/drm/i2c/ch7033.c
@@ -8,8 +8,107 @@
 #include <drm/drm_print.h>
 #include <drm/drm_probe_helper.h>
 
+#define CH7033_PAGE_SEL_REG		0x03
+
+#define CH7033_POWER_STATE_4_REG	0x0a
+#define CH7033_POWER_STATE_4_MEM_INIT		BIT(7)
+#define CH7033_POWER_STATE_4_MEM_STOP		BIT(4)
+
+#define CH7033_INPUT_TIMING_1_REG	0x0b
+#define CH7033_INPUT_TIMING_1_HTI(val)		(((val >> 8) & 0xf) << 3)
+#define CH7033_INPUT_TIMING_1_HAI(val)		((val >> 8) & 0x7)
+
+#define CH7033_INPUT_TIMING_2_REG	0x0c
+#define CH7033_INPUT_TIMING_2_HAI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_3_REG	0x0d
+#define CH7033_INPUT_TIMING_3_HTI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_4_REG	0x0e
+#define CH7033_INPUT_TIMING_4_HWI(val)		(((val >> 8) & 0x7) << 3)
+#define CH7033_INPUT_TIMING_4_HOI(val)		((val >> 8) & 0x7)
+
+#define CH7033_INPUT_TIMING_5_REG	0x0f
+#define CH7033_INPUT_TIMING_5_HOI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_6_REG	0x10
+#define CH7033_INPUT_TIMING_6_HWI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_7_REG	0x11
+#define CH7033_INPUT_TIMING_7_VTI(val)		(((val >> 8) & 0x7) << 3)
+#define CH7033_INPUT_TIMING_7_VAI(val)		((val >> 8) & 0x7)
+
+#define CH7033_INPUT_TIMING_8_REG	0x12
+#define CH7033_INPUT_TIMING_8_VAI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_9_REG	0x13
+#define CH7033_INPUT_TIMING_9_VTI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_10_REG	0x14
+#define CH7033_INPUT_TIMING_10_VWI(val)		(((val >> 8) & 0x7) << 3)
+#define CH7033_INPUT_TIMING_10_VOI(val)		((val >> 8) & 0x7)
+
+#define CH7033_INPUT_TIMING_11_REG	0x15
+#define CH7033_INPUT_TIMING_11_VOI(val)		(val & 0xff)
+
+#define CH7033_INPUT_TIMING_12_REG	0x16
+#define CH7033_INPUT_TIMING_12_VWI(val)		(val & 0xff)
+
+#define CH7033_INPUT_POL_REG		0x19
+#define CH7033_INPUT_POL_HSYNC_HI		BIT(5)
+#define CH7033_INPUT_POL_VSYNC_HI		BIT(4)
+#define CH7033_INPUT_POL_DE_HI			BIT(3)
+#define CH7033_INPUT_POL_GCLK(val)		((val >> 16) & 0x3)
+
+#define CH7033_GCLK_1_REG		0x1a
+#define CH7033_GCLK_1_FREQ(val)			((val >> 8) & 0xff)
+
+#define CH7033_GCLK_2_REG		0x1b
+#define CH7033_GCLK_2_FREQ(val)			((val) & 0xff)
+
+#define CH7033_OUTPUT_TIMING_1_REG	0x1f
+#define CH7033_OUTPUT_TIMING_1_HTO(val)		(((val >> 8) & 0xf) << 3)
+#define CH7033_OUTPUT_TIMING_1_HAO(val)		((val >> 8) & 0x7)
+
+#define CH7033_OUTPUT_TIMING_2_REG	0x20
+#define CH7033_OUTPUT_TIMING_2_HAO(val)		(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_3_REG	0x21
+#define CH7033_OUTPUT_TIMING_3_HTO(val)		(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_7_REG	0x25
+#define CH7033_OUTPUT_TIMING_7_VTO(val)		(((val >> 8) & 0x7) << 3)
+#define CH7033_OUTPUT_TIMING_7_VAO(val)		((val >> 8) & 0x7)
+
+#define CH7033_OUTPUT_TIMING_8_REG	0x26
+#define CH7033_OUTPUT_TIMING_8_VAO(val)		(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_9_REG	0x27
+#define CH7033_OUTPUT_TIMING_9_VTO(val)		(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_4_REG	0x54
+#define CH7033_OUTPUT_TIMING_4_HWO(val)		(((val >> 8) & 0x7) << 3)
+#define CH7033_OUTPUT_TIMING_4_HOO(val)		((val >> 8) & 0x7)
+
+#define CH7033_OUTPUT_TIMING_5_REG	0x55
+#define CH7033_OUTPUT_TIMING_5_HOO(val)		(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_6_REG	0x56
+#define CH7033_OUTPUT_TIMING_6_HWO(val)		(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_10_REG	0x57
+#define CH7033_OUTPUT_TIMING_10_VWO(val)	(((val >> 8) & 0x7) << 3)
+#define CH7033_OUTPUT_TIMING_10_VOO(val)	((val >> 8) & 0x7)
+
+#define CH7033_OUTPUT_TIMING_11_REG	0x58
+#define CH7033_OUTPUT_TIMING_11_VOO(val)	(val & 0xff)
+
+#define CH7033_OUTPUT_TIMING_12_REG	0x59
+#define CH7033_OUTPUT_TIMING_12_VWO(val)	(val & 0xff)
+
 struct ch7033_priv {
         struct i2c_client *client;
+	struct i2c_adapter *ddc;
 	struct drm_encoder encoder;
 	struct drm_bridge bridge;
 	struct drm_connector connector;
@@ -22,10 +121,21 @@ struct ch7033_priv {
 #define bridge_to_ch7033_priv(x) \
 	container_of(x, struct ch7033_priv, bridge)
 
+static u8 ch7033_write(struct i2c_client *client, u8 page, u8 reg, u8 value)
+{
+	i2c_smbus_write_byte_data(client, CH7033_PAGE_SEL_REG, page);
+
+	return i2c_smbus_write_byte_data(client, reg, value);
+}
+
 static enum drm_connector_status ch7033_connector_detect(struct drm_connector *connector, bool force)
 {
-	return connector_status_connected;
-	//return connector_status_disconnected;
+	struct ch7033_priv *priv = conn_to_ch7033_priv(connector);
+
+        if (drm_probe_ddc(priv->ddc))
+                return connector_status_connected;
+
+        return connector_status_unknown;
 }
 
 static void ch7033_connector_destroy(struct drm_connector *connector)
@@ -42,26 +152,23 @@ static const struct drm_connector_funcs ch7033_connector_funcs = {
 	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
+
 static int ch7033_connector_get_modes(struct drm_connector *connector)
 {
 	struct ch7033_priv *priv = conn_to_ch7033_priv(connector);
 	struct edid *edid;
-	int n;
+	int ret;
 
-	//edid = drm_do_get_edid(connector, read_edid_block, priv);
-	edid = NULL;
-	if (!edid) {
-		dev_warn(&priv->client->dev, "failed to read EDID\n");
-		return 0;
-	}
-
+	edid = drm_get_edid(connector, priv->ddc);
 	drm_connector_update_edid_property(connector, edid);
-
-	n = drm_add_edid_modes(connector, edid);
-
-	kfree(edid);
-
-	return n;
+	if (edid) {
+		ret = drm_add_edid_modes(connector, edid);
+		kfree(edid);
+	} else {
+		ret = drm_add_modes_noedid(connector, 1920, 1080);
+		drm_set_preferred_mode(connector, 1024, 768);
+	}
+	return ret;
 }
 
 static struct drm_encoder *ch7033_connector_best_encoder(struct drm_connector *connector)
@@ -76,10 +183,6 @@ static const struct drm_connector_helper_funcs ch7033_connector_helper_funcs = {
 	.best_encoder = ch7033_connector_best_encoder,
 };
 
-
-
-
-
 static int ch7033_bridge_attach(struct drm_bridge *bridge)
 {
 	struct ch7033_priv *priv = bridge_to_ch7033_priv(bridge);
@@ -89,11 +192,16 @@ static int ch7033_bridge_attach(struct drm_bridge *bridge)
 	connector->polled = DRM_CONNECTOR_POLL_CONNECT |
 		DRM_CONNECTOR_POLL_DISCONNECT;
 
-	drm_connector_helper_add(connector, &ch7033_connector_helper_funcs);
-	ret = drm_connector_init(bridge->dev, connector, &ch7033_connector_funcs,
-				 DRM_MODE_CONNECTOR_HDMIA);
-	if (ret)
-		return ret;
+        drm_connector_helper_add(connector,
+                                 &ch7033_connector_helper_funcs);
+        ret = drm_connector_init_with_ddc(bridge->dev, connector,
+                                          &ch7033_connector_funcs,
+                                          DRM_MODE_CONNECTOR_DVII,
+                                          priv->ddc);
+        if (ret) {
+                DRM_ERROR("Failed to initialize connector\n");
+                return ret;
+        }
 
 	drm_connector_attach_encoder(&priv->connector,
 				     priv->bridge.encoder);
@@ -111,45 +219,147 @@ static void ch7033_bridge_detach(struct drm_bridge *bridge)
 static enum drm_mode_status ch7033_bridge_mode_valid(struct drm_bridge *bridge,
 				     const struct drm_display_mode *mode)
 {
-//	if (mode->clock > 150000)
-//		return MODE_CLOCK_HIGH;
-//	if (mode->htotal >= BIT(13))
-//		return MODE_BAD_HVALUE;
-//	if (mode->vtotal >= BIT(11))
-//		return MODE_BAD_VVALUE;
+	if (mode->clock > 165000)
+		return MODE_CLOCK_HIGH;
+	if (mode->htotal >= 1920)
+		return MODE_BAD_HVALUE;
+	if (mode->vtotal >= 1080)
+		return MODE_BAD_VVALUE;
 	return MODE_OK;
-}
-
-static void ch7033_bridge_enable(struct drm_bridge *bridge)
-{
-//	struct ch7033_priv *priv = bridge_to_ch7033_priv(bridge);
-}
-
-static void ch7033_bridge_disable(struct drm_bridge *bridge)
-{
-//	struct ch7033_priv *priv = bridge_to_ch7033_priv(bridge);
 }
 
 static void ch7033_bridge_mode_set(struct drm_bridge *bridge,
 				    const struct drm_display_mode *mode,
 				    const struct drm_display_mode *adjusted_mode)
 {
+	struct ch7033_priv *priv = bridge_to_ch7033_priv(bridge);
+	u16 hbp = mode->hsync_start - mode->hdisplay;
+	u16 hsync_len = mode->hsync_end - mode->hsync_start;
+	u16 vbp = mode->vsync_start - mode->vdisplay;
+	u16 vsync_len = mode->vsync_end - mode->vsync_start;
+	u32 val;
+
+	/* Setup the horizontal timings ... */
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_1_REG,
+		     CH7033_INPUT_TIMING_1_HTI(mode->htotal) |
+		     CH7033_INPUT_TIMING_1_HAI(mode->hdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_2_REG,
+		     CH7033_INPUT_TIMING_2_HAI(mode->hdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_3_REG,
+		     CH7033_INPUT_TIMING_3_HTI(mode->htotal));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_4_REG,
+		     CH7033_INPUT_TIMING_4_HOI(hbp) |
+		     CH7033_INPUT_TIMING_4_HWI(hsync_len));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_5_REG,
+		     CH7033_INPUT_TIMING_5_HOI(hbp));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_6_REG,
+		     CH7033_INPUT_TIMING_6_HWI(hsync_len));
+
+	/* ... And the vertical ones */
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_7_REG,
+		     CH7033_INPUT_TIMING_7_VTI(mode->vtotal) |
+		     CH7033_INPUT_TIMING_7_VAI(mode->vdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_8_REG,
+		     CH7033_INPUT_TIMING_8_VAI(mode->vdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_9_REG,
+		     CH7033_INPUT_TIMING_9_VTI(mode->vtotal));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_10_REG,
+		     CH7033_INPUT_TIMING_10_VOI(vbp) |
+		     CH7033_INPUT_TIMING_10_VWI(vsync_len));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_11_REG,
+		     CH7033_INPUT_TIMING_11_VOI(vbp));
+
+	ch7033_write(priv->client, 0, CH7033_INPUT_TIMING_12_REG,
+		     CH7033_INPUT_TIMING_12_VWI(vsync_len));
+
+	/* Setup polarities and clock */
+	val = CH7033_INPUT_POL_DE_HI;
+	val |= (mode->flags & DRM_MODE_FLAG_PHSYNC) ? CH7033_INPUT_POL_HSYNC_HI : 0;
+	val |= (mode->flags & DRM_MODE_FLAG_PVSYNC) ? CH7033_INPUT_POL_VSYNC_HI : 0;
+	val |= CH7033_INPUT_POL_GCLK(mode->clock);
+	ch7033_write(priv->client, 0, CH7033_INPUT_POL_REG, val);
+
+	ch7033_write(priv->client, 0, CH7033_GCLK_1_REG,
+		     CH7033_GCLK_1_FREQ(mode->clock));
+
+	ch7033_write(priv->client, 0, CH7033_GCLK_2_REG,
+		     CH7033_GCLK_2_FREQ(mode->clock));
+
+	/* Horizontal output timings ... */
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_1_REG,
+		     CH7033_OUTPUT_TIMING_1_HTO(mode->htotal) |
+		     CH7033_OUTPUT_TIMING_1_HAO(mode->hdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_2_REG,
+		     CH7033_OUTPUT_TIMING_2_HAO(mode->hdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_3_REG,
+		     CH7033_OUTPUT_TIMING_3_HTO(mode->htotal));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_4_REG,
+		     CH7033_OUTPUT_TIMING_4_HOO(hbp) |
+		     CH7033_OUTPUT_TIMING_4_HWO(hsync_len));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_5_REG,
+		     CH7033_OUTPUT_TIMING_5_HOO(hbp));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_6_REG,
+		     CH7033_OUTPUT_TIMING_6_HWO(hsync_len));
+
+	/* ... And the vertical ones */
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_7_REG,
+		     CH7033_OUTPUT_TIMING_7_VTO(mode->vtotal) |
+		     CH7033_OUTPUT_TIMING_7_VAO(mode->vdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_8_REG,
+		     CH7033_OUTPUT_TIMING_8_VAO(mode->vdisplay));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_9_REG,
+		     CH7033_OUTPUT_TIMING_9_VTO(mode->vtotal));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_10_REG,
+		     CH7033_OUTPUT_TIMING_10_VOO(vbp) |
+		     CH7033_OUTPUT_TIMING_10_VWO(vsync_len));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_11_REG,
+		     CH7033_OUTPUT_TIMING_11_VOO(vbp));
+
+	ch7033_write(priv->client, 0, CH7033_OUTPUT_TIMING_12_REG,
+		     CH7033_OUTPUT_TIMING_12_VWO(vsync_len));
+
+
+	if (mode->htotal >= 1280) {
+		i2c_smbus_write_byte_data(priv->client, 0x03, 0x03);
+		i2c_smbus_write_byte_data(priv->client, 0x28, 0x0c); //i2cset -f -y 1 0x76 0x28 0x0c # 0x04  weird clocking artifacts
+		i2c_smbus_write_byte_data(priv->client, 0x03, 0x00);
+		i2c_smbus_write_byte_data(priv->client, 0x2b, 0x09); //i2cset -f -y 1 0x76 0x2b 0x09 # 0x08 no signal
+	} else {
+		i2c_smbus_write_byte_data(priv->client, 0x03, 0x03);
+		i2c_smbus_write_byte_data(priv->client, 0x28, 0x04); //i2cset -f -y 1 0x76 0x28 0x0c # 0x04  weird clocking artifacts
+		i2c_smbus_write_byte_data(priv->client, 0x03, 0x00);
+		i2c_smbus_write_byte_data(priv->client, 0x2b, 0x08); //i2cset -f -y 1 0x76 0x2b 0x09 # 0x08 no signal
+	}
+
+	i2c_smbus_write_byte_data(priv->client, 0x03, 0x04);
+	i2c_smbus_write_byte_data(priv->client, 0x52, 0xce);
+	i2c_smbus_write_byte_data(priv->client, 0x52, 0xcf);
 }
 
 static const struct drm_bridge_funcs ch7033_bridge_funcs = {
 	.attach = ch7033_bridge_attach,
 	.detach = ch7033_bridge_detach,
 	.mode_valid = ch7033_bridge_mode_valid,
-	.enable = ch7033_bridge_enable,
-	.disable = ch7033_bridge_disable,
 	.mode_set = ch7033_bridge_mode_set,
 };
-
-
-
-
-
-
 
 static void ch7033_encoder_destroy(struct drm_encoder *encoder)
 {
@@ -159,9 +369,6 @@ static void ch7033_encoder_destroy(struct drm_encoder *encoder)
 static const struct drm_encoder_funcs ch7033_encoder_funcs = {
 	.destroy = ch7033_encoder_destroy,
 };
-
-
-
 
 
 static int ch7033_bind(struct device *dev, struct device *master, void *data)
@@ -185,17 +392,12 @@ static int ch7033_bind(struct device *dev, struct device *master, void *data)
 	ret = drm_encoder_init(drm, &priv->encoder, &ch7033_encoder_funcs,
 			       DRM_MODE_ENCODER_TMDS, NULL);
 	if (ret)
-		goto err_encoder;
+		return ret;
 
 	ret = drm_bridge_attach(&priv->encoder, &priv->bridge, NULL);
 	if (ret)
-		goto err_bridge;
+		drm_encoder_cleanup(&priv->encoder);
 
-	return 0;
-
-err_bridge:
-	drm_encoder_cleanup(&priv->encoder);
-err_encoder:
 	return ret;
 }
 
@@ -213,16 +415,32 @@ static const struct component_ops ch7033_ops = {
 };
 
 
-
-
-
-
-
 static int ch7033_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
+	struct device_node *ddc_node, *remote;
 	struct device *dev = &client->dev;
 	struct ch7033_priv *priv;
+	struct i2c_adapter *ddc;
 	int ret;
+
+	remote = of_graph_get_remote_node(dev->of_node, 1, -1);
+	if (!remote) {
+		dev_err(&client->dev, "XXX MISSING REMOTE\n");
+		return -EINVAL;
+	}
+
+	ddc_node = of_parse_phandle(remote, "ddc-i2c-bus", 0);
+	of_node_put(remote);
+	if (!ddc_node) {
+		dev_err(&client->dev, "XXX NO I2C BUS\n");
+		return -ENODEV;
+	}
+
+	ddc = of_get_i2c_adapter_by_node(ddc_node);
+	if (!ddc) {
+		dev_warn(&client->dev, "DEFER: NO I2C\n");
+		return -EPROBE_DEFER;
+	}
 
 	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv)
@@ -233,6 +451,7 @@ static int ch7033_probe(struct i2c_client *client, const struct i2c_device_id *i
 	INIT_LIST_HEAD(&priv->bridge.list);
 
 	priv->client = client;
+	priv->ddc = ddc;
 
 	priv->bridge.funcs = &ch7033_bridge_funcs;
 	priv->bridge.of_node = dev->of_node;
