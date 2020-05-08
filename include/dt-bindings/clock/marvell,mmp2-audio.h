413af2b249c3c5d79695aa9ff74c820de47ccf01
Lubomir Rintel <lkundrak@v3.sk>
2020-05-08 23:49:30 +0200

    dt-bindings: sound: Add Marvell MMP Audio Clock Controller binding
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

---
+++ include/dt-bindings/clock/marvell,mmp2-audio.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-2-Clause) */
+#ifndef __DT_BINDINGS_CLOCK_MARVELL_MMP2_AUDIO_H
+#define __DT_BINDINGS_CLOCK_MARVELL_MMP2_AUDIO_H
+
+#define MMP2_CLK_AUDIO_SYSCLK		1
+#define MMP2_CLK_AUDIO_SSPA0		2
+#define MMP2_CLK_AUDIO_SSPA1		3
+#endif
