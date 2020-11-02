5aba0b1fe4dc8b1af9e54f5abad1e57652b79219
Lubomir Rintel <lkundrak@v3.sk>
2020-11-02 23:58:28 +0100

    x

--- drivers/gpu/drm/bridge/himax-hx8837.c
+++ drivers/gpu/drm/bridge/himax-hx8837.c
@@ -15,6 +15,7 @@
 
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
+#include <linux/pm_runtime.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
 
@@ -106,13 +107,9 @@ static enum drm_mode_status hx8837_bridge_mode_valid(
 static void hx8837_bridge_post_disable(struct drm_bridge *bridge)
 {
 	struct hx8837_priv *priv = bridge_to_hx8837_priv(bridge);
-	int ret;
 
-	ret = regulator_bulk_disable(ARRAY_SIZE(priv->supplies),
-				     priv->supplies);
-	if (ret)
-		dev_err(priv->dev,"cannot disable regulators %d\n", ret);
-
+	printk("==== POST DISABLE ===\n");
+	pm_runtime_put_sync(priv->dev);
 }
 
 static void hx8837_bridge_disable(struct drm_bridge *bridge)
@@ -120,6 +117,7 @@ static void hx8837_bridge_disable(struct drm_bridge *bridge)
 	struct hx8837_priv *priv = bridge_to_hx8837_priv(bridge);
 	int ret;
 
+	printk("==== DISABLE ===\n");
 	ret = gpiod_direction_output(priv->load_gpio, 0);
 	if (ret)
 		dev_err(priv->dev, "error disabling the dcon load: %d\n", ret);
@@ -136,12 +134,9 @@ static void hx8837_bridge_disable(struct drm_bridge *bridge)
 static void hx8837_bridge_pre_enable(struct drm_bridge *bridge)
 {
 	struct hx8837_priv *priv = bridge_to_hx8837_priv(bridge);
-	int ret;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(priv->supplies),
-				    priv->supplies);
-	if (ret)
-		dev_err(priv->dev,"cannot enable regulators %d\n", ret);
+	printk("==== PRE ENABLE ===\n");
+	pm_runtime_get_sync(priv->dev);
 }
 
 static void hx8837_bridge_enable(struct drm_bridge *bridge)
@@ -149,6 +144,7 @@ static void hx8837_bridge_enable(struct drm_bridge *bridge)
 	struct hx8837_priv *priv = bridge_to_hx8837_priv(bridge);
 	int ret;
 
+	printk("==== ENABLE ===\n");
 	ret = regmap_update_bits(priv->regmap, DCON_REG_MODE,
 					       MODE_PASSTHRU |
 					       MODE_SLEEP |
@@ -173,6 +169,7 @@ static void hx8837_bridge_mode_set(struct drm_bridge *bridge,
 {
 	struct hx8837_priv *priv = bridge_to_hx8837_priv(bridge);
 
+	printk("==== MODE SET ===\n");
 	regmap_write(priv->regmap, DCON_REG_HRES, mode->hdisplay);
 	regmap_write(priv->regmap, DCON_REG_HTOTAL, mode->htotal);
 	regmap_write(priv->regmap, DCON_REG_HSYNC_WIDTH,
@@ -278,33 +275,38 @@ static int hx8837_probe(struct i2c_client *client,
 
 	if (panel->backlight) {
 		dev_err(dev, "the panel already has a backlight controller\n");
-		return -ENODEV;
+		//return -ENODEV;
 	}
 
 	priv->panel_bridge = devm_drm_panel_bridge_add(dev, panel);
 	if (IS_ERR(priv->panel_bridge))
 		return PTR_ERR(priv->panel_bridge);
 
+	//pm_runtime_set_active(dev);
+	pm_runtime_enable(dev);
+
 	priv->regmap = devm_regmap_init_i2c(client, &hx8837_regmap_config);
 	if (IS_ERR(priv->regmap)) {
 		dev_err(dev, "regmap_init_i2c failed\n");
-		return PTR_ERR(priv->regmap);
+		ret = PTR_ERR(priv->regmap);
+		goto error;
 	}
 
 	ret = regmap_read(priv->regmap, DCON_REG_ID, &val);
 	if (ret < 0) {
 		dev_err(dev, "error reading the model id: %d\n", ret);
-		return ret;
+		goto error;
 	}
 	if ((val & 0xff00) != 0xdc00) {
 		dev_err(dev, "the device is not a hx8837\n");
-		return -ENODEV;
+		ret = -ENODEV;
+		goto error;
 	}
 
 	ret = regmap_read(priv->regmap, DCON_REG_BRIGHT, &val);
 	if (ret < 0) {
 		dev_err(&bl->dev, "error getting the backlight: %d\n", ret);
-		return ret;
+		goto error;
 	}
 	bl_props.brightness = val & 0xf;
 
@@ -312,7 +314,8 @@ static int hx8837_probe(struct i2c_client *client,
 					    &hx8837_bl_ops, &bl_props);
 	if (IS_ERR(bl)) {
 		dev_err(dev, "failed to register backlight\n");
-		return PTR_ERR(bl);
+		ret = PTR_ERR(bl);
+		goto error;
 	}
 
 	panel->backlight = bl;
@@ -322,7 +325,13 @@ static int hx8837_probe(struct i2c_client *client,
 	priv->bridge.of_node = dev->of_node;
 	drm_bridge_add(&priv->bridge);
 
+	pm_runtime_idle(dev);
 	return 0;
+
+error:
+	pm_runtime_disable(dev);
+	pm_runtime_set_suspended(dev);
+	return ret;
 }
 
 static int hx8837_remove(struct i2c_client *client)
@@ -332,8 +341,50 @@ static int hx8837_remove(struct i2c_client *client)
 
 	drm_bridge_remove(&priv->bridge);
 
+	printk("=== REMOVE ===\n");
+	//pm_runtime_get_sync(dev);
+	pm_runtime_disable(dev);
+	//pm_runtime_set_suspended(dev);
+	//pm_runtime_put_noidle(dev);
+
 	return 0;
 }
+
+static int __maybe_unused hx8837_runtime_suspend(struct device *dev)
+{
+	struct hx8837_priv *priv = dev_get_drvdata(dev);
+	int ret;
+
+	printk("=== RUNTIME SUSPEND ===\n");
+	ret = regulator_bulk_disable(ARRAY_SIZE(priv->supplies),
+				     priv->supplies);
+	if (ret)
+		dev_err(priv->dev,"cannot disable regulators %d\n", ret);
+
+	return ret;
+}
+
+static int __maybe_unused hx8837_runtime_resume(struct device *dev)
+{
+	struct hx8837_priv *priv = dev_get_drvdata(dev);
+	int ret;
+
+	printk("=== RUNTIME RESUME ===\n");
+
+	ret = regulator_bulk_enable(ARRAY_SIZE(priv->supplies),
+				    priv->supplies);
+	if (ret)
+		dev_err(priv->dev,"cannot enable regulators %d\n", ret);
+
+	return ret;
+}
+
+static const struct dev_pm_ops hx8837_pm_ops = {
+        SET_RUNTIME_PM_OPS(hx8837_runtime_suspend,
+			   hx8837_runtime_resume, NULL)
+        SET_SYSTEM_SLEEP_PM_OPS(pm_runtime_force_suspend,
+                                pm_runtime_force_resume)
+};
 
 static const struct of_device_id hx8837_dt_ids[] = {
 	{ .compatible = "himax,hx8837", },
@@ -353,6 +404,7 @@ static struct i2c_driver hx8837_driver = {
 	.driver = {
 		.name = "hx8837",
 		.of_match_table = of_match_ptr(hx8837_dt_ids),
+		.pm = &hx8837_pm_ops,
 	},
 	.id_table = hx8837_ids,
 };
